004a0226c0c37d00a36bfbfd8fae97c8ecd221e3
Christian Bumann <bch@bbtsoftware.ch>
2020-01-31 08:25:44 +0100

    sonarlint-vs2019 initial commit

---
+++ automatic/sonarlint-vs2019/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$file = "$toolsDir\SonarLint.VSIX-4.16.0.14364-2019.vsix"
+
+$packageArgs = @{
+  packageName = $env:ChocolateyPackageName
+  vsixUrl     = 'file://' + $file.Replace('\', '/')
+}
+
+Install-VisualStudioVsixExtension @packageArgs
