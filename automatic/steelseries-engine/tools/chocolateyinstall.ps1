22ba89d377188e73142d5dcdf00f4e7046396721
mazak-ui <github@rcmhq.com>
2021-11-27 13:26:37 -0600

    Update engine

--- automatic/steelseries-engine/tools/chocolateyinstall.ps1
+++ automatic/steelseries-engine/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://engine.steelseriescdn.com/SteelSeriesEngine3.19.2Setup.exe'
-  checksum      = '27899fb6fe11de7957fc758c34990e4dc2b52547ea52e353258723bc3b8fa6e0'
+  url           = 'https://engine.steelseriescdn.com/SteelSeriesEngine3.22.2Setup.exe'
+  checksum      = 'b4e05c613b7a6d931b386db0fa680f97110b1d9f306d473e8b85e955454d9521'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
