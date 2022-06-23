4b59451dea6c0c2178cd27353e45fcdb34f03af4
Andy Boutte <andyboutte@gmail.com>
2022-06-23 12:12:23 -0500

    updating package to get checksum dynamically

--- automatic/helix-alm-client/tools/chocolateyinstall.ps1
+++ automatic/helix-alm-client/tools/chocolateyinstall.ps1
@@ -1,13 +1,17 @@
 ﻿$ErrorActionPreference = 'Stop';
 
+$version = 'r2022.1.0'
+$baseurl = "https://cdist2.perforce.com/alm/helixalm/$version"
+$url = "$baseurl/ttwinclientinstall.exe"
+$checksum64 = ((Invoke-WebRequest "$baseurl/SHA256SUMS" -UseBasicParsing).RawContent.ToString().Split() | Select-String -Pattern 'ttwinclientinstall.exe' -SimpleMatch -Context 1,0 ).ToString().Trim().Split()[0]
+
 $packageArgs = @{
-  packageName  = $env:ChocolateyPackageName
-
-  url          = 'https://cdist2.perforce.com/alm/helixalm/r2022.1.0/ttwinclientinstall.exe'
-  checksum     = 'b5280432d1cc20087ecbaddbf41ab4afc323403885c9c686575e4622cd46ea2c'
-  checksumType = 'sha256'
-
-  silentArgs   = "-i SILENT LAX_VM resource\jre\bin\java.exe"
+  packageName    = $env:ChocolateyPackageName
+  installerType	 = 'EXE'
+  url            = $url
+  checksum       = $checksum64
+  checksumType   = 'sha256'
+  silentArgs	   = "-i SILENT LAX_VM resource\jre\bin\java.exe"
 }
 
 Install-ChocolateyPackage @packageArgs
