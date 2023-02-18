b8c6040503fbc0a9fee12e33b55eea9e96539d5e
Brian <18603393+brian6932@users.noreply.github.com>
2023-02-18 18:21:54 -0500

    sublimemerge: Add `smerge` bin shim

--- automatic/sublimemerge/tools/chocolateyinstall.ps1
+++ automatic/sublimemerge/tools/chocolateyinstall.ps1
@@ -1,14 +1,36 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-    
-  url64         = 'https://download.sublimetext.com/sublime_merge_build_2083_x64_setup.exe'
-  checksum64    = 'e07ca51bb910f301e12e593d273be2951dc930400e97901c6098a31d59be7355'
-  checksumType  = 'sha256'
-  
-  
-  silentArgs	= "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+    packageName    = $env:ChocolateyPackageName
+    softwareName   = 'Sublime Merge'
+    url64          = 'https://download.sublimetext.com/sublime_merge_build_2083_x64_setup.exe'
+    checksum64     = 'e07ca51bb910f301e12e593d273be2951dc930400e97901c6098a31d59be7355'
+    checksumType   = 'sha256'
+    silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+    validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
+
+[array]$key = Get-UninstallRegistryKey @packageArgs
+
+switch ($key.Count) {
+    1 {
+        $key | ForEach-Object {
+            Install-BinFile -Name 'smerge' -Path (Join-Path -Path $_.InstallLocation -ChildPath 'smerge.exe')
+        }
+        break
+    }
+    0 {
+        Write-Error "$env:ChocolateyPackageName installation failed, unable to detect uninstall registry key."
+        Write-Warning "Please report this to the package maintainer."
+        break
+    }
+    default {
+        Write-Error "$env:ChocolateyPackageName installation failed, multiple ($($key.Count)) uninstall registry keys found."
+        Write-Warning "Please inform the package maintainer that the following keys were matched:"
+        $key | ForEach-Object {
+            Write-Warning "- $($_.DisplayName): $($_.InstallLocation)"
+        }
+    }
+}
--- automatic/sublimemerge/tools/chocolateyuninstall.ps1
+++ automatic/sublimemerge/tools/chocolateyuninstall.ps1
@@ -1,30 +1,34 @@
 $ErrorActionPreference = 'Stop'
- 
+
+Uninstall-BinFile -Name 'smerge'
+
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     softwareName   = 'Sublime Merge'
     fileType       = 'exe'
     silentArgs     = '/SILENT /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /TASKS="contextentry"'
- 
     validExitCodes = @(0)
 }
- 
+
 [array]$key = Get-UninstallRegistryKey @packageArgs
- 
-if ($key.Count -eq 1) {
-    $key | ForEach-Object { 
-        $packageArgs['file'] = "$($_.UninstallString)"        
-        Uninstall-ChocolateyPackage @packageArgs
+
+switch ($key.Count) {
+    1 {
+        $key | ForEach-Object {
+            $packageArgs.file = $_.UninstallString.toString()
+            Uninstall-ChocolateyPackage @packageArgs
+        }
+        break
     }
-}
-elseif ($key.Count -eq 0) {
-    Write-Warning "$env:ChocolateyPackageName has already been uninstalled by other means."
-}
-elseif ($key.Count -gt 1) {
-    Write-Warning "$key.Count matches found!"
-    Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-    Write-Warning "Please alert package maintainer the following keys were matched:"
-    $key | ForEach-Object {
-        Write-Warning "- $_.DisplayName"
+    0 {
+        Write-Warning "$env:ChocolateyPackageName has already been uninstalled by other means."
+        break
+    }
+    default {
+        Write-Warning "$($key.Count) matches found!"
+        Write-Warning "Please alert package maintainer the following keys were matched:"
+        $key | ForEach-Object {
+            Write-Warning "- $($_.DisplayName): $($_.InstallLocation)"
+        }
     }
 }
