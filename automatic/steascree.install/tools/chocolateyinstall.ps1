7b3ae67042e19b0a8b98185c6fe7fc7b79cb3a9a
soredake <5204968+soredake@users.noreply.github.com>
2023-04-15 17:32:31 +0300

    steascree.install: fix https://github.com/chtof/chocolatey-packages/issues/90 and add /DesktopShortcut option to create desktop shortcut

--- automatic/steascree.install/tools/chocolateyinstall.ps1
+++ automatic/steascree.install/tools/chocolateyinstall.ps1
@@ -1,10 +1,37 @@
 $ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pp = Get-PackageParameters
+$DesktopShortcutPath = "$env:PUBLIC\Desktop\SteaScree.lnk"
+$url       = 'https://steascree.download/steascree-1.5.4-windows.exe'
+$checksum  = 'ddeb40de6538f47a6666a656c867261d3349ca01d8f5dd1145b8e74574f270d4'
 
 $packageArgs = @{
-  packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\steascree-1.5.4-windows.exe"
-  silentArgs  = '--mode unattended'
+  packageName    = $Env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url            = $url
+  checksum       = $checksum
+  checksumType   = 'sha256'
+  silentArgs     = '--mode unattended'
+}
+Install-ChocolateyPackage @packageArgs
+
+
+function InstallShortcut {
+  param (
+    $ShortcutPath
+  )
+
+  $installLocation = "${env:ProgramFiles(x86)}\SteaScree"
+
+  $shortcutArgs = @{
+    shortcutFilePath = $ShortcutPath
+    workingDirectory = "$installLocation"
+    targetPath       = "$installLocation\SteaScree.exe"
+  }
+
+  Install-ChocolateyShortcut @shortcutArgs
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+if ($pp['DesktopShortcut']) {
+    InstallShortcut $DesktopShortcutPath
+}
