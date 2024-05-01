3c307c4ed0d5805eac1bb760741ddb34ae07a7f5
chtof <chtof62@gmail.com>
2024-05-02 01:23:44 +0200

    Update update.ps1

--- automatic/epidata/update.ps1
+++ automatic/epidata/update.ps1
@@ -3,19 +3,22 @@ import-module au
 
 function global:au_GetLatest {
 	$releases = 'http://www.epidata.dk/download.php'
-	$regex    = 'setup.all-in-one.(?<Version>[\d\.]+).exe'
+	$regex    = 'all-in-one?.?(-setup\.)(?<Version>[\d\.]+).exe$'
 
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	$url = $download_page.links | ? href -match $regex
     $version = $matches.Version
 
-    return @{ Version = $matches.Version ; URL32 = 'http://www.epidata.dk/downloads/setup.all-in-one.' + $version + '.exe' }
+    return @{
+        Version = $matches.Version
+        URL32 = 'http://www.epidata.dk/downloads/epidata-all-in-one-setup.' + $version + '.exe'
+    }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+			"(^(\s)*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
