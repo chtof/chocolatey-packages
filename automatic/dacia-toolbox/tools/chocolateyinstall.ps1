0703b066377ce63df145acf2ccc11c56b9217bd5
chtof <chtof62@gmail.com>
2024-05-01 23:13:28 +0200

    Update chocolateyinstall.ps1

--- automatic/dacia-toolbox/tools/chocolateyinstall.ps1
+++ automatic/dacia-toolbox/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://download.naviextras.com/content/!application/TOOLBOX/Agents/ReDa_ULC_4/2024_04_10_10_53/Dacia/DaciaMediaNavEvolutionToolbox-inst.exe'
-  checksum     = '76e77d982b65c83e442a12bb61fedddce980ba3a45e792cfbb38625be28beb57'
+  checksum     = '76E77D982B65C83E442A12BB61FEDDDCE980BA3A45E792CFBB38625BE28BEB57'
   checksumType = 'sha256'
 
   silentArgs   = "/S"
