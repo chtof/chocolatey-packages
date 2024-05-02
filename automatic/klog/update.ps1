0cc4a0e31d31e5a5853eaf900133dccde817e552
chtof <chtof62@gmail.com>
2024-05-02 12:10:40 +0200

    Update update.ps1

--- automatic/klog/update.ps1
+++ automatic/klog/update.ps1
@@ -6,23 +6,21 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {
    return github_GetInfo -ArgumentList @{
         repository = 'ea4k/klog'
-        regex32    = 'KLog-(.*)-windows-installer.exe$'        
+        regex64    = 'KLog-(.*)-(windows-installer|win64).exe$'        
    }
 }
 
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL32)"
-            "(?i)(Get-RemoteChecksum ).*" = "`${1}$($Latest.URL32)"
-            "(?i)(checksum type:\s+).*"   = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"         = "`${1} $($Latest.Checksum32)"
+            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"
+            "(?i)(Get-RemoteChecksum ).*" = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*"   = "`${1}$($Latest.ChecksumType64)"            
             "(?i)(checksum64:).*"         = "`${1} $($Latest.Checksum32)"
         }
 
         "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }
