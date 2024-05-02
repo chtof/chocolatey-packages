b40815e8d9eb7969a856beed37c32c683bc74904
chtof <chtof62@gmail.com>
2024-05-02 22:02:21 +0200

    Update chocolateyinstall.ps1

--- automatic/chirp.portable/tools/chocolateyinstall.ps1
+++ automatic/chirp.portable/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
 }
 
 Get-ChocolateyUnzip @packageArgs
-Remove-Item -Path $packageArgs.file
+Remove-Item -Path $packageArgs.file64
 
 # Install start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
