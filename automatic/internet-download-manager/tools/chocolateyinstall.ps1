bf7d0113c1564e29f971574ef487d983ba622766
Chocolatey <chocolatey@realdimensions.net>
2024-05-04 16:28:19 +0000

    AU: 4 updated - chirp chirp.install chirp.portable internet-download-manager [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/35443be76ff6317b2533b471f5226aefe33f1446

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman642build9.exe'
-  checksum      = '8b84f664b307f5e29e4697356bf481153f5bc0f451385a4daa000ed9270700d4'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman642build10.exe'
+  checksum      = '7bc13cbe154bb9afea8f11e38e8343a914efc8f627d102650a3ac66ac85d5f57'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
