893c98ce96de487598c17d65b6b794c991935107
Chocolatey <chocolatey@realdimensions.net>
2024-05-09 16:28:35 +0000

    AU: 2 updated - bandlab-assistant vcvrack [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/400bf9bfe6a3e2f373a93b356084c6354f0bf694

--- automatic/bandlab-assistant/tools/chocolateyinstall.ps1
+++ automatic/bandlab-assistant/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.2.6.exe'
-  checksum      = 'b4d8c231c0ac4e92168091d826099b98b353458dea63c7696b6151110382f7db'
+  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.2.7.exe'
+  checksum      = 'e8c6044477af15c5372d26be8decc7d93ccaca8c24898558c9c940b8587ceb87'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
