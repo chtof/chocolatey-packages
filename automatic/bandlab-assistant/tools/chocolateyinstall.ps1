f02c200957f83265bb3aad0cae5006d56e305d7e
Chocolatey <chocolatey@realdimensions.net>
2024-05-23 16:41:19 +0000

    AU: 2 updated - bandlab-assistant genymotion [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/577a8e9af96779ec3f1063913d1949011cb4bcd7

--- automatic/bandlab-assistant/tools/chocolateyinstall.ps1
+++ automatic/bandlab-assistant/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.4.0.exe'
-  checksum      = '7b83fbb11b89a9f8e533706ae4fa7b4138ae191bf50d94cd0249df3536b93783'
+  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.4.1.exe'
+  checksum      = '536aaa6e7ac1dea2f9e834703557685f40b14952d35319afeeba01b82820ac87'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
--- automatic/genymotion/tools/chocolateyinstall.ps1
+++ automatic/genymotion/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://dl.genymotion.com/releases/genymotion-3.6.0/genymotion-3.6.0.exe'
-  checksum     = '@{Algorithm=SHA256; Hash=FB8F02459CB5EF092B23EC44BA1FCE797A5672E00D57ECF6EDA13CF92317A02F; Path=C:\Users\appveyor\AppData\Local\Temp\1\genymotion.exe}'
+  url          = 'https://dl.genymotion.com/releases/genymotion-3.7.0/genymotion-3.7.0.exe'
+  checksum     = '@{Algorithm=SHA256; Hash=C866E07583FCC9AC903FFFF674B50527CC73C43ED6DF086B9C80D187370F41E9; Path=C:\Users\appveyor\AppData\Local\Temp\1\genymotion.exe}'
   checksumType = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
