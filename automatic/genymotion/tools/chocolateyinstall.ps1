e669ece1488943c6eacb3605d87763b6a6ff7d16
Chocolatey <chocolatey@realdimensions.net>
2024-05-28 16:41:41 +0000

    AU: 1 updated - genymotion [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/51591f10e10e37dcbadf85ad9c23057de5bf7562

--- automatic/genymotion/tools/chocolateyinstall.ps1
+++ automatic/genymotion/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://dl.genymotion.com/releases/genymotion-3.7.0/genymotion-3.7.0.exe'
-  checksum     = '@{Algorithm=SHA256; Hash=C866E07583FCC9AC903FFFF674B50527CC73C43ED6DF086B9C80D187370F41E9; Path=C:\Users\appveyor\AppData\Local\Temp\1\genymotion.exe}'
+  url          = 'https://dl.genymotion.com/releases/genymotion-3.7.1/genymotion-3.7.1.exe'
+  checksum     = '@{Algorithm=SHA256; Hash=C6A37AB7452D021B7589288602C2D171024D47C648BD3D9A55E105720CFCEBDC; Path=C:\Users\appveyor\AppData\Local\Temp\1\genymotion.exe}'
   checksumType = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
