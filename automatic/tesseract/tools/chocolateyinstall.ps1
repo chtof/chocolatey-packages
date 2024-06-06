dc699c252c212055f6869ab39542006ecf33ecc6
Chocolatey <chocolatey@realdimensions.net>
2024-06-06 16:29:16 +0000

    AU: 4 updated - chirp chirp.install chirp.portable tesseract [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/bfe09557b5763305d348059e4e3ce8c6137ffd4b

--- automatic/tesseract/tools/chocolateyinstall.ps1
+++ automatic/tesseract/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName  
-  file64      = "$toolsDir\tesseract-ocr-w64-setup-5.3.4.20240503.exe"
+  file64      = "$toolsDir\tesseract-ocr-w64-setup-5.4.0.20240606.exe"
   silentArgs  = '/S'
 }
 
