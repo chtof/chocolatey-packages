77f042fee53e22c145c59b9313569c466ac7fdd0
Chocolatey <chocolatey@realdimensions.net>
2024-06-15 08:27:44 +0000

    AU: 3 updated - multipar multipar.install multipar.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/2e3bf48b98675e73a3a996d9e20b4507e70490d6

--- automatic/multipar.install/tools/chocolateyinstall.ps1
+++ automatic/multipar.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   destination   = "$toolsDir"
-  file64        = "$toolsDir\MultiPar1332_setup.exe"
+  file64        = "$toolsDir\MultiPar1333_setup.exe"
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
