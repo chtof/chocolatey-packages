34ea32f0db7f7b7ddca70bd2b21e2535ead505c5
Chocolatey <chocolatey@realdimensions.net>
2024-06-26 08:29:11 +0000

    AU: 2 updated - steelseries-engine virtualhere-server [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/496a2b0b3309a8c1394132da2590532ced19388a

--- automatic/steelseries-engine/tools/chocolateyinstall.ps1
+++ automatic/steelseries-engine/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG64.0.0Setup.exe'
-  checksum      = 'de1dff855aaf10f81ce9312b9f406ea620e1d43bb69d71bb3aa122b3eed30f81'
+  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG65.0.0Setup.exe'
+  checksum      = '6ba17f233ed1f7a6f1877d6601649fe9b9b6e621bc337c0b35881367d929e64f'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
--- automatic/virtualhere-server/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-server/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   fileFullPath   = "$toolsDir\vhusbdwin$(Get-OSArchitectureWidth).exe"
 
   url64          = 'https://virtualhere.com/sites/default/files/usbserver/vhusbdwin64.exe'
-  checksum64     = 'bbdfdab1d631a4d7797d65b920049207dee88229342c166b5c9f6d47e70ffecd'
+  checksum64     = '6144e25fbe27924ab91673455bb1c7c495e7071c4bac1b990e52c13f934eb685'
   checksumType64 = 'sha256'
 }
 
