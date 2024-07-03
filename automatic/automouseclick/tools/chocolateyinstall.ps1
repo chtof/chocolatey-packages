f4c742cd28ca402b69ef79a814e667b0923f1a16
Chocolatey <chocolatey@realdimensions.net>
2024-07-03 00:26:51 +0000

    AU: 2 updated - automouseclick awscli-session-manager [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/eb69b43db2621695e29466c0b8a73f7c7f866f05

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'CE3B42AADBB44489B1741AE4BBDC5D29D526C1EC209B81F97811AA93C5C1324E'
+  checksum     = '2D906FF460C9BAEE2E61CAB888AE818537E6EA8286AC5A47D07F4699EA0AEA49'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
