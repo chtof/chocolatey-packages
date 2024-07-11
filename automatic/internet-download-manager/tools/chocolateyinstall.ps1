ce8b4848321c990a6cb8bfe298735c3b06391a1b
Chocolatey <chocolatey@realdimensions.net>
2024-07-11 00:27:56 +0000

    AU: 2 updated - internet-download-manager steelseries-engine [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/c53fb3bf27bc4b91ea9b06263d6eb66ee06b840b

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman642build12.exe'
-  checksum      = '9d01cc78df9756d61c7d134d6d0b6760128ac87c50acf7dae654614af0a40636'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman642build14.exe'
+  checksum      = 'd87951945c7ec774741dcd0416ff3457e2d85a573ae8694951b928db3ed2b2de'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
--- automatic/steelseries-engine/tools/chocolateyinstall.ps1
+++ automatic/steelseries-engine/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG65.0.0Setup.exe'
-  checksum      = '6ba17f233ed1f7a6f1877d6601649fe9b9b6e621bc337c0b35881367d929e64f'
+  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG66.0.0Setup.exe'
+  checksum      = 'b48981cacda09af3e47a1bf322949b7a12749f93a2fbc2b63b33aae5563ad848'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
