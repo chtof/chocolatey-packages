8f963e77a4011749c48910f24c471563c2cdfb6d
Chocolatey <chocolatey@realdimensions.net>
2024-07-26 08:29:20 +0000

    AU: 1 updated - virtualhere-client [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/e42856d37babbf44abff6d38d18223b3a3af85c9

--- automatic/virtualhere-client/tools/chocolateyinstall.ps1
+++ automatic/virtualhere-client/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   fileFullPath   = "$toolsDir\vhui$(Get-OSArchitectureWidth).exe"
 
   url64          = 'https://www.virtualhere.com/sites/default/files/usbclient/vhui64.exe'
-  checksum64     = 'e331d711e2be4b7eef5f1e203e813b4ae56d0607493e200560b0986c39877f02'
+  checksum64     = 'd8f18137c5bae51b41812a3fbaee42c01c28f555b9bfd46a4b0e0651b3170ce8'
   checksumType64 = 'sha256'
 }
 
