fc23ed9c10bf08f4de9050f3cc7694911c396485
Chocolatey <chocolatey@realdimensions.net>
2024-08-01 00:30:22 +0000

    AU: 2 updated - internet-download-manager klayout [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/3f741289a3d8bd239259d6eea6674967b56d839c

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman642build17.exe'
-  checksum      = '6fb3984acc4c678d8b6c1b3c4826dff677f4ca48f1ce1b799432e613d90a9731'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman642build18.exe'
+  checksum      = '10175f8480a170eea40c1cf31c3b6dfde6da63ddfd59383c661acba7723bc8b2'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
--- automatic/klayout/tools/chocolateyinstall.ps1
+++ automatic/klayout/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.29.4-win32-install.exe'
-  checksum      = '28b26f75f6195dcf1ca1f1b5352bdd5d0a8cb124386f4894c543e8778e5198cc'
+  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.29.5-win32-install.exe'
+  checksum      = '2d7074d136c60d4b77f8225d3aaa6b804202c2a4ec2faeff46b429bed2822f98'
   checksumType  = 'sha256'
 
-  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.29.4-win64-install.exe'
-  checksum64    = '1f3b5b862c74f8d2efd44a718f5cb4b0c4aeb7c6e1551812cf69a0b09d796097'
+  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.29.5-win64-install.exe'
+  checksum64    = '18f417f04f7cc907b5d9fbad0f91b556d6c2608136d0aecbf3331d894a077171'
 
   checksumType64= 'sha256'
 
