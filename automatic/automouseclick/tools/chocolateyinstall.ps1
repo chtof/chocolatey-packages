b38f250fc80646b76caa396b3fb9f2b578ade860
Chocolatey <chocolatey@realdimensions.net>
2024-08-06 00:27:40 +0000

    AU: 4 updated - apo-ok apo-ok.install apo-ok.portable automouseclick [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/70dbb3dd906abeaf1a366cf6f19b38b1489156b5

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'F2C529F599C52FC43B4A321B40B6AE333CED748F19F8AC410680249E9AD97AD7'
+  checksum     = '0A9E304ABF8F053B7FDB80EA2DC58CED6FD352DA22CDB575D54AD182FEC4C321'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
