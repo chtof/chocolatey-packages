0d74ac7f8e0171ef676dab8f99d2d3752adac753
Chocolatey <chocolatey@realdimensions.net>
2024-08-16 00:28:37 +0000

    AU: 2 updated - automouseclick klayout [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/21ddf3e7aa429c59d7b0702e8876a05b3395f0ab

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = 'EF35498617AB08E4E48F0FF9368FB6CD43012280881AE912BE0562377F3E7E0F'
+  checksum     = '6E9DA58796172408302CCA4F529B50F74366F59A681630BB32F8D6978817742A'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/klayout/tools/chocolateyinstall.ps1
+++ automatic/klayout/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.29.5-win32-install.exe'
-  checksum      = '2d7074d136c60d4b77f8225d3aaa6b804202c2a4ec2faeff46b429bed2822f98'
+  url           = 'https://www.klayout.org/downloads/Windows/klayout-0.29.6-win32-install.exe'
+  checksum      = '22177e9316f35cf94fa0b6b21549135c78e3c5cb7d81ef53fb8af0aa140ff794'
   checksumType  = 'sha256'
 
-  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.29.5-win64-install.exe'
-  checksum64    = '18f417f04f7cc907b5d9fbad0f91b556d6c2608136d0aecbf3331d894a077171'
+  url64         = 'https://www.klayout.org/downloads/Windows/klayout-0.29.6-win64-install.exe'
+  checksum64    = 'dd0de081aff01f3f34e377a2b89d5984433150843219089cdb2f84396e664793'
 
   checksumType64= 'sha256'
 
