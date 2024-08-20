53199a6cb485c1706c7429942ff6e9983a386dad
Chocolatey <chocolatey@realdimensions.net>
2024-08-20 08:28:08 +0000

    AU: 1 updated - bandlab-assistant [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/e4a53ddb863728fa6e85c832449bcba871c8c420

--- automatic/bandlab-assistant/tools/chocolateyinstall.ps1
+++ automatic/bandlab-assistant/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.8.1.exe'
-  checksum      = 'db251625d70b6d313a936fb2cab1f0663dc4297afc3bf1215cb5a8ec7d51648a'
+  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.8.2.exe'
+  checksum      = '7a008919069e001e26260a9bc66c64ebf7be58299fb9aba735d2378e873b6b19'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
