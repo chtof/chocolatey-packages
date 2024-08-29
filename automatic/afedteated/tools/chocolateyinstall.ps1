564879befbf821d567224b942dbea7b8d26181ab
Chocolatey <chocolatey@realdimensions.net>
2024-08-29 08:28:32 +0000

    AU: 3 updated - afedteated geogebra6 geogebra-geometry [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/43927563df731d398fd110693ebb3b66a5e1dd87

--- automatic/afedteated/tools/chocolateyinstall.ps1
+++ automatic/afedteated/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://cdn.iobit.com/dl/advanced-systemcare-setup.exe'
-  checksum     = 'a0832a346b29ff200f57cf76570ea98491745b5173ae16d1759c8bdd7803326222bf7f41259ed8c80c59bbed39dc51635e5ea336983af871debde87a4fc6daac'
+  checksum     = '618eec541f5c3f2283514e64836acb33586b2ba4cc91a1e293af2f4ad1e827c27c33abeafcf7dd19dd75075bd3c13952f64b4e8a403a18af1f001aae1f95c91f'
   checksumType = 'sha512'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
