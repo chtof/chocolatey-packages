4c2ea9cee256fa5218f33e8c603538745e2c2821
Chocolatey <chocolatey@realdimensions.net>
2024-08-29 00:27:28 +0000

    AU: 4 updated - automouseclick ocenaudio ocenaudio.install ocenaudio.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/44a538bf430aa7c71ab17358cb48a1f3da81ec43

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '7B0C01114302968537A5FFEF1239B4A9F47FA399D7DCF709497E8B8BA2DA1A92'
+  checksum     = 'F02794513A1D0D168BFF271E5FEF37EE258C6EC67B7FF2698F700E22A1C56ECF'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
