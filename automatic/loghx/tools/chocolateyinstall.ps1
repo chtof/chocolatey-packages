1ac750185d79765c0721b45390f327b1aff45b11
Chocolatey <chocolatey@realdimensions.net>
2024-09-06 08:27:43 +0000

    AU: 1 updated - loghx [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/6f4c2d66eca7565a563d841b7d42ce45c8023633

--- automatic/loghx/tools/chocolateyinstall.ps1
+++ automatic/loghx/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_766.exe'
-  checksum      = '11984cfb65bad3aa1fe1e323606111b60a8924c8d70ecb83337e213662bb1ee1'
+  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_767.exe'
+  checksum      = '93962bf64f72d870e11e712edfccb789dfed4f0be8760849461bc1e21ea1fb26'
   checksumType  = 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
