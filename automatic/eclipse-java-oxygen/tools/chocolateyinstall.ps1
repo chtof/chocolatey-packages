1d0e1d17bccb394f0d232d46c83e52dfa3febd80
Chocolatey <chocolatey@realdimensions.net>
2024-09-13 16:27:41 +0000

    AU: 3 updated - eclipse-java-oxygen geogebra6 geogebra-geometry [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/2d2dea0acc1357d0247f02ef2e1d5942a613d9f0

--- automatic/eclipse-java-oxygen/tools/chocolateyinstall.ps1
+++ automatic/eclipse-java-oxygen/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir"
 
-  url           = 'http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/2024-06/R/eclipse-java-2024-06-R-win32-x86_64.zip'
-  checksum      = '58acb3d2fbdaddac86ec2cbe94221b0ae5d9065aa5a6e06c64b7c0e5b4d4be64'
+  url           = 'http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/2024-09/R/eclipse-java-2024-09-R-win32-x86_64.zip'
+  checksum      = 'b94f1fa4c0e737a0a603fb53d20fe11932d6ebc0bb4bed5575f595e3df8f3a18'
   checksumType  = 'sha256'
 }
 
