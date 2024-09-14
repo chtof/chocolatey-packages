8601b7cf3edb15543dd48c688213ca59a5acaef4
Chocolatey <chocolatey@realdimensions.net>
2024-09-14 16:53:52 +0000

    AU: 1 updated - basilisk.install [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/b614aaf01238c0eac49919672ab0cd80c522ead2

--- automatic/basilisk.install/tools/chocolateyinstall.ps1
+++ automatic/basilisk.install/tools/chocolateyinstall.ps1
@@ -3,12 +3,12 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
     
-  url            = 'http://archive.palemoon.org/basilisk/basilisk-20201125140449.win32.installer.exe'
-  checksum       = '05240bb65daf43fdda01cc45b483a4a9326cd3f8e4f2123e8ec7d0b7ec958341'
+  url            = 'https://archive.basilisk-browser.org/2024.09.13/windows/x86_32/basilisk-20240912184256.win32.installer.exe'
+  checksum       = 'b636c7743011cd5623650f3b22319f052dde8222d41323793f36366383d44516'
   checksumType   = 'sha256'
 
-  url64          = 'http://archive.palemoon.org/basilisk/basilisk-20220127135138.win64.installer.exe'
-  checksum64     = '3324e597f3a8a4686f4dd4db8f76a8c91cbc1230c536f85d8122c5b2be903701'
+  url64          = 'https://archive.basilisk-browser.org/2024.09.13/windows/x86_64/basilisk-20240912184157.win64.installer.exe'
+  checksum64     = 'ad189e59f85f3f88dbef7eda9ad31ee493ef80697ebed7cc64392ee7f6555cda'
   checksumType64 = 'sha256'
 
   silentArgs     = "/S"
