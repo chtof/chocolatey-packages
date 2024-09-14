7579d44f4856bc14febf1220731ba478ebd39b0f
chtof <chtof62@gmail.com>
2024-09-14 18:20:27 +0200

    Update update.ps1

--- automatic/basilisk/update.ps1
+++ automatic/basilisk/update.ps1
@@ -1,23 +1,11 @@
-﻿import-module au
-
-function global:au_GetLatest {
-    $releases = 'https://basilisk-browser.org/download.shtml'    
-    $regex64  = 'basilisk-(?<Version>[\d]+)[\d]{6}.win64.installer.exe'
-
-    $download_page = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links
-    $download_page |? href -match $regex64 | Select -Last 1 | Out-Null        
-
-    return @{
-        Version = $matches.Version -replace '([\d]+)([\d]{2})([\d]{2})','$1.$2.$3'    
-    }
-}
+﻿. $PSScriptRoot\..\basilisk.install\update.ps1
 
 function global:au_SearchReplace {
    @{
         "$($Latest.PackageName).nuspec" = @{
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""            
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
         }
     }
 }
 
-update -ChecksumFor none -NoCheckUrl
+update -ChecksumFor none
