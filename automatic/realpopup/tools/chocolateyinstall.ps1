b3010d9ee8801f1f2fa996f8cc2991ffcbc07780
Chocolatey <chocolatey@realdimensions.net>
2024-09-24 08:27:43 +0000

    AU: 2 updated - mboxviewer realpopup [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/9aac06df45b7eeb4c286314d07f9268160b9d742

--- automatic/realpopup/tools/chocolateyinstall.ps1
+++ automatic/realpopup/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype     = 'MSI'
 
   url          = 'https://www.realpopup.it/files/RealPopupSetup.msi'
-  checksum     = 'd2538d11a21b5fcfbd002d9848e4bd2e260bcce02eef3f70b86ed6c3ba24a160'
+  checksum     = '37d0f12dd4c01603e198ac359ff1774988855eb06f7670e5ea3516b019a2f8fb'
   checksumType = 'sha256'
 
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
