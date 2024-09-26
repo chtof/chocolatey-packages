5826a6f398d60089ccd0e5383a821e0797ba935c
Chocolatey <chocolatey@realdimensions.net>
2024-09-26 16:29:07 +0000

    AU: 3 updated - chirp chirp.install chirp.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/03e79060c29396effee119074f6e86635937f058

--- automatic/chirp.portable/tools/chocolateyinstall.ps1
+++ automatic/chirp.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = "$env:ChocolateyPackageName"
   destination   = "$toolsDir"
-  file64        = "$toolsDir\chirp-next-20240919-win64.zip"
+  file64        = "$toolsDir\chirp-next-20240926-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
