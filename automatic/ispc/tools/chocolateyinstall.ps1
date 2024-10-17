a0e1157559af1dcf6a7c93a5dcb868708473158a
Chocolatey <chocolatey@realdimensions.net>
2024-10-17 16:28:10 +0000

    AU: 1 updated - ispc [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/ddefaa1a555c299fb34b561272531c721829ef23

--- automatic/ispc/tools/chocolateyinstall.ps1
+++ automatic/ispc/tools/chocolateyinstall.ps1
@@ -5,14 +5,14 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   packageName = $env:ChocolateyPackageName
   fileType    = 'MSI'
 
-  file        = "$toolsDir\ispc-v1.25.0-windows.zip"
+  file        = "$toolsDir\ispc-v1.25.1-windows.zip"
   silentArgs   = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" AUTOOC=0"
 }#>
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\ispc-v1.25.0-windows.zip"  
+  file        = "$toolsDir\ispc-v1.25.1-windows.zip"  
 }
 
 Get-ChocolateyUnzip @packageArgs
