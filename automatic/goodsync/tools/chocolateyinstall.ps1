31eadd8184c813d72b9aeb3f7c72dac24b7071d0
Chocolatey <chocolatey@realdimensions.net>
2024-10-25 16:27:58 +0000

    AU: 4 updated - chirp chirp.install chirp.portable goodsync [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/33909ce837a480e53660331337316e15dad6bfab

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-vsub-Server-Setup.exe'
-  checksum       = '859ff9d831378900761bb319d6fdb0ca3a8682b71890c6e4989d448c3f4412e3'
+  checksum       = 'eb5bc9e6e97e42113af7a6e87a3030bf61d1f26eddd8dc7f78a69c3ce921cd9b'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
