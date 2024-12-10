a9d75a8dbd89f21b20d6deb940a476fc551515bf
Chocolatey <chocolatey@realdimensions.net>
2024-12-10 00:41:25 +0000

    AU: 1 updated - hashtools [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/697b03bd7c36fdc72b129a9dd0121ada3ab0117d

--- automatic/hashtools/tools/chocolateyinstall.ps1
+++ automatic/hashtools/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://binaryfortressdownloads.com/Download/BFSFiles/109/HashToolsSetup-4.8.1.exe'
-  checksum     = '3aad0784f5d63247dd1b6a104731facda499cb4d46bb5da9c74b5c316ca5a193'
+  url          = 'https://binaryfortressdownloads.com/Download/BFSFiles/109/HashToolsSetup-4.8.2.exe'
+  checksum     = '17208e0015d636dea1ea035771dd5ccb181c60985c183619951f3527f1ba869f'
   checksumType = 'sha256'
   
   silentArgs   = '/VERYSILENT /SILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'
