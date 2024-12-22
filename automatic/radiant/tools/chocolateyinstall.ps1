d3e1e1270d38f0e73cedfb361e9276199c4fb0a0
Chocolatey <chocolatey@realdimensions.net>
2024-12-22 16:41:16 +0000

    AU: 1 updated - radiant [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/e60741ed29d59a7754939cf6c88075dcf801c803

--- automatic/radiant/tools/chocolateyinstall.ps1
+++ automatic/radiant/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://www.radiantviewer.com/files/RadiAnt-2024.1-Setup.exe'
-  checksum      = '64d9f20671f0a30cfdb9326c964622efe793d4d44bf208c6167ab7a7337ec967'
+  url           = 'https://www.radiantviewer.com/files/RadiAnt-2024.2-Setup.exe'
+  checksum      = '43b81c4a6d16e7ef1d789663a567e0adfa12b1878776b45623eb235e16a6f15f'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"
