c24c28d54dc2f41b48e09b3ded1fdc824a686f53
Chocolatey <chocolatey@realdimensions.net>
2025-01-08 08:41:08 +0000

    AU: 2 updated - afedteated loghx [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/de5b4d4543dfb841e3e23a7bdcdcd58e4b6d8771

--- automatic/afedteated/tools/chocolateyinstall.ps1
+++ automatic/afedteated/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://cdn.iobit.com/dl/advanced-systemcare-setup.exe'
-  checksum     = '67a836501bddbf9345993ecdaf0abf1fe06dafc462f33df1a05e7df102b33b1ef880895d8d1a6aad9793a1cabf7e10d30200f506e6edf00fee0df0bed97c7af9'
+  checksum     = 'd7375d48d9040670b87b7486d8f76477c3e97d7cfe02daeb04f64703a19fbfa7abd447b06e3f1eb6366aec83086f996bbbbd31cd218c8654d02bc4dbe473aaff'
   checksumType = 'sha512'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
--- automatic/loghx/tools/chocolateyinstall.ps1
+++ automatic/loghx/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_772.exe'
-  checksum      = 'a174ac26a9ebc871b7d801038daddc4e1f511b5fe0419d6a1d621fb7acbdf40c'
+  url           = 'http://rx4hx.qrz.ru/files/loghx/LogHX3Setup_build_774.exe'
+  checksum      = '79e636a282016b9df7295665e4f65eb2b59bd8167be2d3acb74af75fedf4e948'
   checksumType  = 'sha256'
 
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
