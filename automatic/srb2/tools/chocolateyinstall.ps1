b649618399802dfa2fefc52b4a455626ffe3c92b
Chocolatey <chocolatey@realdimensions.net>
2025-01-18 00:41:07 +0000

    AU: 1 updated - srb2 [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/2fbf468070cf911538c6b478def4a4a41e1b2c07

--- automatic/srb2/tools/chocolateyinstall.ps1
+++ automatic/srb2/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url           = 'https://github.com//STJr/SRB2/releases/download/SRB2_release_2.2.13/SRB2-v2213-Installer.exe'
-  checksum      = '74da34ca3ba1962015714afa8535051b3ca8414ace28b0bdc13a176f6218d703'
+  url           = 'https://github.com//STJr/SRB2/releases/download/SRB2_release_2.2.14/SRB2-v2214-Installer.exe'
+  checksum      = '3f0725fbc19acfb4c05cb73d50058c7d64855adcacfc7ba757802ad468b20013'
   checksumType  = 'sha256'
   
   
