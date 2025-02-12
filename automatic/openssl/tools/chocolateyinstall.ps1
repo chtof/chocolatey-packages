9d8d398082539959872b3734d425100cc4e22c52
Chocolatey <chocolatey@realdimensions.net>
2025-02-12 00:41:19 +0000

    AU: 1 updated - openssl [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/1d40670829596ebf899c5a2aacb18693ca337037

--- automatic/openssl/tools/chocolateyinstall.ps1
+++ automatic/openssl/tools/chocolateyinstall.ps1
@@ -3,13 +3,13 @@
 $packageArgs = @{
   packageName     = $env:ChocolateyPackageName
   
-  url32           = 'https://slproweb.com/download/Win32OpenSSL-3_4_0.exe'  
+  url32           = 'https://slproweb.com/download/Win32OpenSSL-3_4_1.exe'  
   checksumType32  = 'sha512'
-  checksum32      = 'e624d34b86dcf2895321ab966369b17168ded98c376bc8333ec01cf6cfde7a20057aa40a61dbb6593fb05acf3eb8d00d26f3dc4f65476297d2aa0114767e0cb2'
+  checksum32      = '58ebad01d586c432410a40257f0105b4101184471f621ca154c7904a402d69dd74b39d1e43618551898227420e0a80c638c93f8b135ae08f2857e939ab7c1422'
 
-  url64           = 'https://slproweb.com/download/Win64OpenSSL-3_4_0.exe'
+  url64           = 'https://slproweb.com/download/Win64OpenSSL-3_4_1.exe'
   checksumType64  = 'sha512'
-  checksum64      = 'b50ee0ce50bac108959854b0e4612217abe1004936058941be757b90dc683658449cbee9b91d84e54a706982ba4ac028eff7fcf388c775b35d76cde895c4364f'
+  checksum64      = '5d897ecfd1687c23e5d7f0371d11939bc755274bf4ac7b4b4ed80e4582ee4d7b764b2944d136e8dfd86e300f025231a6cdbc408cb7e83c454eb46b07dc8bfa70'
   silentArgs      = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
