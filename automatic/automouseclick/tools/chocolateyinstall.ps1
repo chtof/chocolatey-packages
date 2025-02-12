0c6bbb9a46c4a0ed27d10569a13b14809ede9dcb
Chocolatey <chocolatey@realdimensions.net>
2025-02-12 16:41:48 +0000

    AU: 2 updated - automouseclick moneydance [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/a1aa12ea0c7003f8bde554dea987741b2c6cfb38

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '273465140EE779C33DF10797DF54312B2300D7C6BDC57C4F25002944CEAE127E'
+  checksum     = '61D34F0E035DEC41712751E7F8BEBC8103E4F038D1FD8D14B2F6A0C9900BC4DB'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/moneydance/tools/chocolateyinstall.ps1
+++ automatic/moneydance/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
   url64          = 'https://infinitekind.com/stabledl/current/Moneydance_windows_amd64.exe'
-  checksum64     = '1b647267ab489bc36f4c4205cb0065daae99ddf8c2479f92531a0f5fcbb8f7f6'
+  checksum64     = '77064fbb306abf9fed11fdbef05b281f3303e208b91932ce5d6fac42952fa3f6'
   checksumType64 = 'sha256'
 
   silentArgs     = "-q"
