858a7ad1dd55d69611744748b67f9f07943b233c
Chocolatey <chocolatey@realdimensions.net>
2025-02-18 16:41:36 +0000

    AU: 2 updated - geogebra6 geogebra-geometry [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/644fca9245a301f76924afb89df6617f733d5294

--- automatic/geogebra-geometry/tools/chocolateyinstall.ps1
+++ automatic/geogebra-geometry/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file     = 'GeoGebraGeometry-Windows-Installer-6-0-874-1.exe'
+$file     = 'GeoGebraGeometry-Windows-Installer-6-0-875-1.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
