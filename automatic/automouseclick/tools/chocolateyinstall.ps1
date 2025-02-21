5628a00da2cf20c4601c18b599e4ab6f5ad9427e
Chocolatey <chocolatey@realdimensions.net>
2025-02-21 16:47:16 +0000

    AU: 4 updated - automouseclick chirp chirp.install chirp.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/e5fe848cd5a5cb6c3f4c737bffc1d7dad574c752

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '189C13218F69E09FF63AA448B26355176D35E0B6CE49D999D25A5F829DEDF1F7'
+  checksum     = '912FE195B3A86AF8CE3C4EAB22C436578E3497B1E3E7CCD79036577AF484F0C0'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/chirp.portable/tools/chocolateyinstall.ps1
+++ automatic/chirp.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = "$env:ChocolateyPackageName"
   destination   = "$toolsDir"
-  file64        = "$toolsDir\chirp-next-20250214-win64.zip"
+  file64        = "$toolsDir\chirp-next-20250221-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
