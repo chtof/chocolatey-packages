3041bef66f1e1c93cd6d9527280cd38ea5270464
Chocolatey <chocolatey@realdimensions.net>
2025-02-22 08:41:17 +0000

    AU: 1 updated - moneydance [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/26a9a098b8ab23cc75311b5554f8e7a08565be7c

--- automatic/moneydance/tools/chocolateyinstall.ps1
+++ automatic/moneydance/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
 
   url64          = 'https://infinitekind.com/stabledl/current/Moneydance_windows_amd64.exe'
-  checksum64     = '77064fbb306abf9fed11fdbef05b281f3303e208b91932ce5d6fac42952fa3f6'
+  checksum64     = '4cbb9d76b1d3fd191941fa1a83cc7bdd9592e4ab9248d2817b7ef5474182e249'
   checksumType64 = 'sha256'
 
   silentArgs     = "-q"
