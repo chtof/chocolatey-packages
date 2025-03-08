e39d2f3cad46178154d4d7b6001c11f623e965c5
chtof <chtof62@gmail.com>
2025-03-08 22:53:05 +0100

    Update update.ps1

--- automatic/kdevelop.portable/update.ps1
+++ automatic/kdevelop.portable/update.ps1
@@ -9,7 +9,7 @@ function global:au_GetLatest {
     $url     = $download_page.links | ? href -match $regex
     $version = $matches.Version -Replace '-', '.'
  
-    return @{ Version = $version ; URL64 = $releases + $url.href }
+    return @{ Version = "5.6.2."+$version ; URL64 = $releases + $url.href }
 }
 
 function global:au_SearchReplace {
