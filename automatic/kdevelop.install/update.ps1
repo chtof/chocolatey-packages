12ecfbbba874fe3a1c49e925ae44f673d330396a
chtof <chtof62@gmail.com>
2025-03-08 22:40:55 +0100

    Update update.ps1

--- automatic/kdevelop.install/update.ps1
+++ automatic/kdevelop.install/update.ps1
@@ -1,10 +1,10 @@
 ﻿import-module au
 
 function global:au_GetLatest {
-    $releases = 'https://binary-factory.kde.org/job/KDevelop_Release_win64/lastSuccessfulBuild/artifact/'
+    $releases = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/'
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
     #https://binary-factory.kde.org/job/KDevelop_Release_win64/lastSuccessfulBuild/artifact/kdevelop-5.6.2-1853-windows-cl-msvc2019-x86_64.exe
-    $regex   = 'kdevelop-(?<Version>[\d\.-]+)-windows-cl-msvc2019-x86_64.exe$'
+    $regex   = 'kdevelop-master-(?<Version>[\d\.-]+)-windows-cl-msvc\d+-x86_64.exe$'
     $url     = $download_page.links | ? href -match $regex
     $version = $matches.Version -Replace '-', '.'
  
