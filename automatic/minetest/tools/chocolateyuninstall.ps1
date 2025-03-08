f8f1c0e59851d132ab883e5579e519be66cb0842
chtof <chtof62@gmail.com>
2025-03-08 23:04:32 +0100

    Update chocolateyuninstall.ps1

--- automatic/minetest/tools/chocolateyuninstall.ps1
+++ automatic/minetest/tools/chocolateyuninstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop'
 # Remove start menu shortcut
 $programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path $programs "Minetest.lnk"
+$shortcutFilePath = Join-Path $programs "Luanti.lnk"
 if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
