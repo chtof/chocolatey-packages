4618f6d901f3a42c4ea2434d31ba2bbca265aeed
chtof <chtof62@gmail.com>
2025-03-08 22:44:09 +0100

    Update chocolateyinstall.ps1

--- automatic/kdevelop.portable/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.portable/tools/chocolateyinstall.ps1
@@ -3,8 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = "kdevelop.portable"
-  destination   = "$toolsDir"
-  file          = "$toolsDir\kdevelop-5.1.2-x86-portable.7z"
+  destination   = "$toolsDir"  
   file64        = "$toolsDir\kdevelop-5.1.2-x64-portable.7z"
 }
 
