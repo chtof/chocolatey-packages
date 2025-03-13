732ca3db24249da28daf2e29f2ab2723ce2ac705
Chocolatey <chocolatey@realdimensions.net>
2025-03-13 00:41:23 +0000

    AU: 3 updated - kdevelop kdevelop.install mboxviewer [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/d9c1ac1a20c8330bbcf263545943d3e7cc2b3042

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5319-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = 'f4aae4ab9587a2b797fbb3b10299218600b8f331f8204d537dc85824bf8b5b69'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5326-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = '1e3aba5b703a414f0f850adb1b0f243009d4827c16a850c27832825602532c90'
   checksumType64 = 'sha256'
 }
 
