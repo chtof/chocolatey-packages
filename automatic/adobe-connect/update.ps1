628149959fc945f7aeb65b9f7a22730c4a80687a
chtof <chtof62@gmail.com>
2025-03-16 12:26:31 +0100

    Update update.ps1

--- automatic/adobe-connect/update.ps1
+++ automatic/adobe-connect/update.ps1
@@ -13,7 +13,8 @@ function global:au_GetLatest {
     $regex_url32     = 'https://www.adobe.com/(.*)?go/(?<url_suffix32>Connect(\d+)_32msi)'
     $regex_url64     = 'https://www.adobe.com/(.*)?go/(?<url_suffix64>Connect(\d+)msi)'
 
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing -Headers @{"authority"="helpx.adobe.com" ;"method"="GET"; "path"="/fr/adobe-connect/connect-downloads-updates.html"  ;"accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8";"accept-encoding"="gzip, deflate, br, zstd"; "scheme"="https" ; "referer"="https://helpx.adobe.com/in/adobe-connect/connect-downloads-updates.html";"sec-ch-ua"="`"Not(A:Brand`";v=`"99`", `"Brave`";v=`"133`", `"Chromium`";v=`"133`"";"sec-fetch-site"="same-origin";"sec-ch-ua-platform"="`"Windows`""}
+
     $download_page -match "$regex_version.64" | Out-Null
     $version = $matches.Version -Replace '_','.'
 
