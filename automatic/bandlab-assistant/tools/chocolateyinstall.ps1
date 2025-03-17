e8ed8abc177e1343834ebf844712c3aaf4883408
Chocolatey <chocolatey@realdimensions.net>
2025-03-17 16:41:51 +0000

    AU: 3 updated - bandlab-assistant geogebra6 geogebra-geometry [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/ec851c7cc54610565f739cd4494db46d1d516e7f

--- automatic/bandlab-assistant/tools/chocolateyinstall.ps1
+++ automatic/bandlab-assistant/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.8.5.exe'
-  checksum      = 'ca62d17125d23a3579ac01eca67d9db419acac98ef697339345f646527cb81d7'
+  url           = 'https://static.bandlab.com/assistant/BandLab%20Assistant%20Setup%2010.8.6.exe'
+  checksum      = 'd4209fdfacc8adc36eed5c1974fbbd3b86b0e89da41ce14c41e50633b9dafd65'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
