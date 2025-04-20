64864e05f7fa138742dd15ce0a1556071b362659
chtof <chtof62@gmail.com>
2025-04-21 00:31:24 +0200

    Update chocolateyinstall.ps1

--- automatic/jack/tools/chocolateyinstall.ps1
+++ automatic/jack/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  destination   = "$toolsDir"
+  fileType      = 'exe'
   file          = "$toolsDir\jack2-win32-v1.9.22.exe"
   file64        = "$toolsDir\jack2-win64-v1.9.22.exe"  
 }
