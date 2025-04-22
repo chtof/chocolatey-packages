8aea56754437849d1865af35553808fe0a4444af
Chocolatey <chocolatey@realdimensions.net>
2025-04-22 16:29:06 +0000

    AU: 5 updated - kdevelop kdevelop.install phonerlite phonerlite.install phonerlite.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/ce551fb94e670a85c096a45d9f20b369acffcef1

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5461-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = 'a30521b1c01f2cc88954e0ff0f4a7d262c3f9c039ab49b356948c65c5c3c3961'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5473-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = 'a6eac7d71e88aff12c2f842cc06d425a7b393f13fd7db6c0ae445d10900b2a29'
   checksumType64 = 'sha256'
 }
 
