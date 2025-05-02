4805b27eb240a7a6908be7fe523c682eec5408b3
Chocolatey <chocolatey@realdimensions.net>
2025-05-02 16:27:39 +0000

    AU: 3 updated - chirp chirp.install chirp.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/b30aec497311cfe0a0cef393b44cec854a61e5d4

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-next-20250425-installer.exe"
+  file          = "$toolsDir\chirp-next-20250502-installer.exe"
   silentArgs    = '/S'
 }
 
