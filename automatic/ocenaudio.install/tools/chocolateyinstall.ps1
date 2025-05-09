3a255f5deb63ff751a8d40221bbf98d08da50de9
chtof <chtof62@gmail.com>
2025-05-09 23:27:48 +0200

    Update chocolateyinstall.ps1

--- automatic/ocenaudio.install/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.install/tools/chocolateyinstall.ps1
@@ -3,11 +3,7 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
     
-  url            = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio.exe'
-  checksum       = '3a8d80a904b37103ec03d0226acfa75addc287201e02def009cd0c7f32b57586'
-  checksumType   = 'sha256'
-
-  url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio64.exe'
+  url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_windows64.exe'
   checksum64     = '3a8d80a904b37103ec03d0226acfa75addc287201e02def009cd0c7f32b57586'
   checksumType64 = 'sha256'
 
