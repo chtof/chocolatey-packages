2d0917632afa1e9a084ced7ebbb28bad82f1891c
Chocolatey <chocolatey@realdimensions.net>
2025-05-13 16:27:40 +0000

    AU: 3 updated - ffmpeg-batch ffmpeg-batch.install genymotion [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/ba7027e60c5cfab7bbd459f333d7c6ff3cf79de3

--- automatic/genymotion/tools/chocolateyinstall.ps1
+++ automatic/genymotion/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://dl.genymotion.com/releases/genymotion-3.8.0/genymotion-3.8.0.exe'
-  checksum     = '@{Algorithm=SHA256; Hash=1D50B5D5916F071CBB2205C9E4E164B83B37F249AD90B8D2AA3A984989331BF5; Path=C:\Users\appveyor\AppData\Local\Temp\1\genymotion.exe}'
+  url          = 'https://dl.genymotion.com/releases/genymotion-3.9.0/genymotion-3.9.0.exe'
+  checksum     = '@{Algorithm=SHA256; Hash=C1E21B7A353EAE01AAF5495D2281D9C15FD3AD91DC404984675A6826BF7A0E96; Path=C:\Users\appveyor\AppData\Local\Temp\1\genymotion.exe}'
   checksumType = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
