ce02aba20c4c7f7e00cd975ecdeabfe1fb95eac7
Chocolatey <chocolatey@realdimensions.net>
2025-05-23 08:28:35 +0000

    AU: 2 updated - log4om.portable yara [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/758e3e445174da13ea9b8b25883773718928c91b

--- automatic/yara/tools/chocolateyinstall.ps1
+++ automatic/yara/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  file        = "$toolsDir\yara-v4.5.2-2326-win32.zip"
-  file64      = "$toolsDir\yara-v4.5.2-2326-win64.zip"
+  file        = "$toolsDir\yara-v4.5.3-2352-win32.zip"
+  file64      = "$toolsDir\yara-v4.5.3-2352-win64.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
