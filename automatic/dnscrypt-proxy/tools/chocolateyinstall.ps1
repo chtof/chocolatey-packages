1549f3a913a23880f9c879427a605134faeb51ce
Chocolatey <chocolatey@realdimensions.net>
2025-05-27 00:28:02 +0000

    AU: 2 updated - dnscrypt-proxy internet-download-manager [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/67864d850a13a4258bbe5e9d451677763b836483

--- automatic/dnscrypt-proxy/tools/chocolateyinstall.ps1
+++ automatic/dnscrypt-proxy/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   destination    = "$toolsDir"
-  filefullpath   = "$toolsDir\dnscrypt-proxy-win32-2.1.11.zip"
-  filefullpath64 = "$toolsDir\dnscrypt-proxy-win64-2.1.11.zip"
+  filefullpath   = "$toolsDir\dnscrypt-proxy-win32-2.1.12.zip"
+  filefullpath64 = "$toolsDir\dnscrypt-proxy-win64-2.1.12.zip"
 }
 
 Get-ChocolateyUnzip @packageArgs
--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman642build38.exe'
-  checksum      = '49f862f66a39600d1778fc29ef3a8c8f3c1854a4b3eaa17e5a9ae445ad50c3e3'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman642build39.exe'
+  checksum      = '51d9348c450b0197afe265e766ef9d3f51083a484ac842b70d15988410629dc6'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
