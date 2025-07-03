4b0013ad1dd564d38a6bcaacf509f18555ffa936
Chocolatey <chocolatey@realdimensions.net>
2025-07-03 00:28:38 +0000

    AU: 4 updated - extraterm extraterm.install extraterm.portable kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/d6ecabd4d72bc890836d7dc07b46ac2007ea0603

--- automatic/extraterm.install/tools/chocolateyinstall.ps1
+++ automatic/extraterm.install/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName  
-  file        = "$toolsDir\extratermqt-setup-0.81.2.exe"
+  file        = "$toolsDir\extratermqt-setup-0.81.3.exe"
   silentArgs  = "/S"
 }
 
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://cdn.kde.org/ci-builds/office/kmymoney/5.2/windows/kmymoney-5.2-4434-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://cdn.kde.org/ci-builds/office/kmymoney/5.2/windows/kmymoney-5.2-4444-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '46c256ac3b66133567820bb36d20394dd7e7f04799c8ba819d1c1b3fd54392df'
+  checksum64     = 'eabbd12183c0eefb32f96f12f759c7663733c71bc549808e965431fc507d6d33'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
