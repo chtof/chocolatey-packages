2804da36939f9d0625ee718037448b1386915952
Chocolatey <chocolatey@realdimensions.net>
2025-07-08 00:28:11 +0000

    AU: 3 updated - kdevelop kdevelop.install kmymoney [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/171e25af0041f2608380a2b46530cb5a17d972e8

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5724-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = '9334eae086c55d67a2da102ed594b17cee3f988786a8da81239475e7f36f58a8'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5730-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = '9d0ae471a7734c6fa4fc6da91dd39f6092f72146e10965fec4d061ea00949349'
   checksumType64 = 'sha256'
 }
 
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://cdn.kde.org/ci-builds/office/kmymoney/5.2/windows/kmymoney-5.2-4467-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://cdn.kde.org/ci-builds/office/kmymoney/5.2/windows/kmymoney-5.2-4477-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '6bfadce3cea47adf2d7fffada7f48d5dff9a6ee44beafb3835e6d98639948469'
+  checksum64     = '22343f423fa84aae7bc843212929578aaa1ddebeb3bac02c9f1379eed99ea026'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
