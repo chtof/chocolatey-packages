f30bfffe34b8440337b736cf5823c3d34e49a818
Chocolatey <chocolatey@realdimensions.net>
2025-07-15 08:28:35 +0000

    AU: 5 updated - dontsleep dontsleep.install dontsleep.portable kmymoney magicsplat-tcl-tk [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/203e1efd3179a0b01c783e8e5e193d44bfb461b1

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://cdn.kde.org/ci-builds/office/kmymoney/5.2/windows/kmymoney-5.2-4499-windows-cl-msvc2019-x86_64.exe'  
+  url64          = 'https://cdn.kde.org/ci-builds/office/kmymoney/5.2/windows/kmymoney-5.2-4505-windows-cl-msvc2019-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '8dcccb9bd38cd4a90714f9a150ad2810b6945c231dea2b7dcc6b9e6d7354d970'
+  checksum64     = 'eefe0ad87269f885f61a901318e725941ab1824575bfb2ff0d251650d2bf6d43'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
