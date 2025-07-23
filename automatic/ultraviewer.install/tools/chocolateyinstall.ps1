c5df7a37302bac1fa472fc34e12ffd6126fd0deb
Chocolatey <chocolatey@realdimensions.net>
2025-07-23 16:29:50 +0000

    AU: 2 updated - ultraviewer ultraviewer.install [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/7df1c073a1d745a3de13eaf7f7bb2b597facbe48

--- automatic/ultraviewer.install/tools/chocolateyinstall.ps1
+++ automatic/ultraviewer.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.ultraviewer.net/en/UltraViewer_setup_6.6_en.exe'
-  checksum     = '169468ed8f7c755f834a7b2f95f077de5f0c94e79c9e1dcc63e9483616a9033a'
+  checksum     = '020aac79a14717e316a593155ca778dd3e253b888bf62633b174838d35df7f41'
   checksumType = 'sha256'  
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
