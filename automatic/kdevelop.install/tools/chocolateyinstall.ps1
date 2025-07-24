92071fef079a0e240fca0434126f8caa1cc95f75
Chocolatey <chocolatey@realdimensions.net>
2025-07-24 00:28:45 +0000

    AU: 2 updated - kdevelop kdevelop.install [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/cec6c88edeeb09d3a325c21c4ed91e72e4206432

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5768-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = 'f67568f99e2d5ccc53e6359011df9039d13066a61e2082820e3b2e8b63bc764d'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5772-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = 'a1a8e38343b931f7ce3b7cc40fe6e31094c830bf0ae9de15a7fada1916368942'
   checksumType64 = 'sha256'
 }
 
