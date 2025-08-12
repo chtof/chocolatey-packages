453a6215c6402d5e5711b103d7c7c230682fea9f
Chocolatey <chocolatey@realdimensions.net>
2025-08-12 16:29:50 +0000

    AU: 2 updated - automouseclick steelseries-engine [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/ac86e6dbff941acdbd50a9c31ffdd4670c621c3f

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '53A4E7A9D3AB3403FFB7165589BA97794E71B98CDE0E337EE1549DAD4F5C3CED'
+  checksum     = 'CBFC25FACDE7CA7628E2D6BE807BFA2AC716C173096695603D73409221FE71C1'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
--- automatic/steelseries-engine/tools/chocolateyinstall.ps1
+++ automatic/steelseries-engine/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG91.0.0Setup.exe'
-  checksum      = 'fec4442739bc3afbcd78dcdba1732858ff6b4d4e9b4363a119eaecbe6da3323d'
+  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG92.0.0Setup.exe'
+  checksum      = '9d1f846b99cfb3ecf53a9d3e3779085208c796c2b7d4fd1d2687d655cd87587a'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
