7fe34028cb2fc5eb7bcf320a691718c8770e0f26
Chocolatey <chocolatey@realdimensions.net>
2025-09-15 16:30:26 +0000

    AU: 1 updated - adobe-connect [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/606b4c5335420540d4c8b48213710f76080363a9

--- automatic/adobe-connect/tools/chocolateyinstall.ps1
+++ automatic/adobe-connect/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
 
   url32         = 'https://download.adobe.com/pub/connect/updaters/meeting/11_0/ConnectApp11_2024_9_149_32.msi'
   checksum32    = '77f153a4ee286b9b90588c6961373ada80f4b67abca0ff74596f3b4884bb382e'
-  url64         = 'https://download.adobe.com/pub/connect/updaters/meeting/11_0/ConnectApp11_2025_5_5.msi'
-  checksum64    = 'bb7a3a12de0c8ffe00fbefdb3a973b18cad4f5684ff2f12f8ae695d66e239bb1'
+  url64         = 'https://download.adobe.com/pub/connect/updaters/meeting/11_0/ConnectApp11_2025_8_189.msi'
+  checksum64    = '44b3d5b4011f4e505e5a34757f8e5f8ba5e877e465712a896eddbc4fb93fcbc9'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
