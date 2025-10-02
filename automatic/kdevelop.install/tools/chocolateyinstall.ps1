dd627eab7fa3ace62fe3f0c07e332ec649c8f5c0
Chocolatey <chocolatey@realdimensions.net>
2025-10-02 00:27:45 +0000

    AU: 5 updated - dontsleep dontsleep.install dontsleep.portable kdevelop kdevelop.install [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/86278047e4e67ff50f7a2400c4b7f10e93c9670a

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5966-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = 'de62cf6ba4a241eb2285608ee1c42e1a1c506d9e00ce0d09b6c7c74a8d8c95f3'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5973-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = '175e35e47ca93c288908c0d616db0c3815109cab933b6f3e44eae990e7d83a58'
   checksumType64 = 'sha256'
 }
 
