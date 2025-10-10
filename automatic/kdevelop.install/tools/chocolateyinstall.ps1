a6f5fe866dd86f7dc18b0c5ff292a7a9c19a5819
Chocolatey <chocolatey@realdimensions.net>
2025-10-10 16:30:15 +0000

    AU: 10 updated - chirp chirp.install chirp.portable dmde kdevelop kdevelop.install phonerlite phonerlite.install phonerlite.portable sd [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/25b59a83f42880c4bfb0999c7518f677e3fc7381

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-5973-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = '175e35e47ca93c288908c0d616db0c3815109cab933b6f3e44eae990e7d83a58'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6005-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = '2bef78820bbff8de83d0c624528b2be63d061c4c61b4c02aaac1f07c1fe6fbb9'
   checksumType64 = 'sha256'
 }
 
