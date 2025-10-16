2b536fa3870836112d82f5f18163eb63a3caadaf
Chocolatey <chocolatey@realdimensions.net>
2025-10-16 16:31:17 +0000

    AU: 3 updated - adobe-connect kmymoney spacedesk-server [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/38a27b6a48f27d36943ee9de11897e12a559c8c5

--- automatic/adobe-connect/tools/chocolateyinstall.ps1
+++ automatic/adobe-connect/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
 
   url32         = 'https://download.adobe.com/pub/connect/updaters/meeting/11_0/ConnectApp11_2024_9_149_32.msi'
   checksum32    = '77f153a4ee286b9b90588c6961373ada80f4b67abca0ff74596f3b4884bb382e'
-  url64         = 'https://download.adobe.com/pub/connect/updaters/meeting/11_0/ConnectApp11_2025_8_189.msi'
-  checksum64    = '44b3d5b4011f4e505e5a34757f8e5f8ba5e877e465712a896eddbc4fb93fcbc9'
+  url64         = 'https://download.adobe.com/pub/connect/updaters/meeting/11_0/ConnectApp11_2025_9_15.msi'
+  checksum64    = '64c3ced0e678b24a2bd405f60a875265b174863950239c0cba2aa837aa477f7c'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://cdn.kde.org/ci-builds/office/kmymoney/5.2/windows/kmymoney-5.2-4878-windows-cl-msvc2022-x86_64.exe'  
+  url64          = 'https://cdn.kde.org/ci-builds/office/kmymoney/5.2/windows/kmymoney-5.2-4889-windows-cl-msvc2022-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '87b133dd9d2d9162dfbf93a04cb133f92d45068153e36909bd34ff65a045a705'
+  checksum64     = 'cc810bcc8b9bd8baee1265430d96b1c974d8077d7979e3d05f4563f3bd5d3bfb'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
