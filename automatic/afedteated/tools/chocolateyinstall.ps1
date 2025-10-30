0d9c5169efac5b44c365de20a48ed8109cb54818
Chocolatey <chocolatey@realdimensions.net>
2025-10-30 08:27:36 +0000

    AU: 1 updated - afedteated [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/e79839f4b3a4e0005a6568b3067e5ac48194b1b7

--- automatic/afedteated/tools/chocolateyinstall.ps1
+++ automatic/afedteated/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://cdn.iobit.com/dl/advanced-systemcare-setup.exe'
-  checksum     = 'efc5f717c8730ef0078ce7592ea53a1c45747954662ee00599c8d99c1870f3f15ac13fef1aaa77d03d24181d3794692a18ae1caa1b215b0c14c182ed971858f4'
+  checksum     = 'b0a34406af372123f2a196e34c70917359c81141443298a492bc57261fdf0d7357bb6dbfb537087697b941a5b85d96e79827aa63640ce3b9434905b96e1a3bcf'
   checksumType = 'sha512'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
