ef711beba333955c6ac42b97ee503f0a6f5c0a9a
Chocolatey <chocolatey@realdimensions.net>
2025-10-31 16:28:09 +0000

    AU: 6 updated - chirp chirp.install chirp.portable dontsleep dontsleep.install dontsleep.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/8e4e6dc1120e949e7a460c99d3eb3b5922f0ff3e

--- automatic/chirp.install/tools/chocolateyinstall.ps1
+++ automatic/chirp.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName  
   fileType      = 'exe'
-  file          = "$toolsDir\chirp-next-20251024-installer.exe"
+  file          = "$toolsDir\chirp-next-20251031-installer.exe"
   silentArgs    = '/S'
 }
 
