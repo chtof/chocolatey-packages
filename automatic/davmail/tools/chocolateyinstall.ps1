b22a8ed1aceb5adaeffd0090257a08aa73f498b5
Chocolatey <chocolatey@realdimensions.net>
2025-11-01 16:28:33 +0000

    AU: 1 updated - davmail [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/954031dda2f1e7eded597c28576f0e04bd2e5a14

--- automatic/davmail/tools/chocolateyinstall.ps1
+++ automatic/davmail/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\davmail-6.4.0-3755-setup.exe"
-  file64      = "$toolsDir\davmail-6.4.0-3755-setup64.exe"
+  file        = "$toolsDir\davmail-6.5.1-3873-setup.exe"
+  file64      = "$toolsDir\davmail-6.5.1-3873-setup64.exe"
   silentArgs  = "/S"
 }
 
