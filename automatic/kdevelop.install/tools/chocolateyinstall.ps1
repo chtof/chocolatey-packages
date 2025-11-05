77bd69613cea3367ed1542ebb9972f1305ac974c
Chocolatey <chocolatey@realdimensions.net>
2025-11-05 16:31:29 +0000

    AU: 3 updated - kdevelop kdevelop.install steelseries-engine [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/fd1abf5bd1b7fca909eff88c3e0ba457bdfc8065

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6078-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = '017436c3326ff6815c765bbcee4dbb7b0a0fa103484f35c381ef409ea96ad2ac'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6086-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = '2e1871c77ed4de142c4581b5cf5cd6b7a53b443891d3da1628932835772fc493'
   checksumType64 = 'sha256'
 }
 
--- automatic/steelseries-engine/tools/chocolateyinstall.ps1
+++ automatic/steelseries-engine/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG97.1.0Setup.exe'
-  checksum      = 'f2a0f040ba42db2a926a23f0871016e9a5106e391da54f1f3b143b96a1a14921'
+  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG98.0.0Setup.exe'
+  checksum      = 'fddd98d97f61965be7522a057c5a4801895bbeb67a432e59fa73dca2233af9ae'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
