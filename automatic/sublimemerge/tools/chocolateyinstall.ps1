08ec2d65352bed8976e2076800a9962100dedf74
Chocolatey <chocolatey@realdimensions.net>
2025-11-06 08:27:57 +0000

    AU: 1 updated - sublimemerge [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/820edf6706f19c62d1912b531c419482b5f0e98b

--- automatic/sublimemerge/tools/chocolateyinstall.ps1
+++ automatic/sublimemerge/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url64         = 'https://download.sublimetext.com/sublime_merge_build_2115_x64_setup.exe'
-  checksum64    = '8a0a56a1158420d64cf4ded0670f8d9b7990037c63ccc533b9aefdcca7638e18'
+  url64         = 'https://download.sublimetext.com/sublime_merge_build_2116_x64_setup.exe'
+  checksum64    = 'e72b4583eaf0d2a53478031a0119fe30e15e917bc63e9187d8196ce1e05fd8a4'
   checksumType  = 'sha256'
    
   silentArgs	= "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
