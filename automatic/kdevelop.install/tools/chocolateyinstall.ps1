9e3de5d66636b8bfb6c09d505ea35af3ce38374f
Chocolatey <chocolatey@realdimensions.net>
2025-11-19 08:41:27 +0000

    AU: 3 updated - kdevelop kdevelop.install spacedesk-server [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/cd1aeb66cec5b5b9b9cf92832b2c178c117b4a76

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6126-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = '992cbd3279322b9976a920512bfc00fbf9e4a11daeea38644a869fe7fd13c095'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6160-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = 'ce8fc51db3ab66e6313d640b527a93863edca7e9de428f6a243c3d7792e1c5a3'
   checksumType64 = 'sha256'
 }
 
