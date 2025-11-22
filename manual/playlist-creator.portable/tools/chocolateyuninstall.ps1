ec696889f45751b8858e098e52651c19baa18e92
chtof <chtof62@gmail.com>
2025-11-22 23:02:56 +0100

    Deprecated

--- manual/playlist-creator.portable/tools/chocolateyuninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿# Remove start menu shortcut
-$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
-$shortcutFilePath = Join-Path $programs "Playlist Creator.lnk"
-if (Test-Path $shortcutFilePath) { Remove-Item $shortcutFilePath }
