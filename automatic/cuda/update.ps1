b9c4203b129780adcffd30484c9d72290fd6d540
chtof <chtof62@gmail.com>
2025-11-23 00:12:26 +0100

    Update update.ps1

--- automatic/cuda/update.ps1
+++ automatic/cuda/update.ps1
@@ -2,15 +2,16 @@
 
 function global:au_GetLatest {
     $releases  = 'https://developer.nvidia.com/cuda-downloads.json'
-    $regex_url = "https://developer.download.nvidia.com/compute/cuda/(?<MajorVersion>[\d\.]+)/local_installers/cuda_(?<VersionMajor>[\d\.]+)_(?<VersionMinor>[\d]+)(.\d+)?_windows.exe"
+    #$regex_url = "https://developer.download.nvidia.com/compute/cuda/(?<MajorVersion>[\d\.]+)/local_installers/cuda_(?<VersionMajor>[\d\.]+)_(?<VersionMinor>[\d]+)(.\d+)?_windows.exe"
+    $regex_url = "https://developer.download.nvidia.com/compute/cuda/(?<MajorVersion>[\d\.]+)/local_installers/cuda_(?<VersionMajor>[\d\.]+)_?(?<VersionMinor>[\d]+)(.\d+)?_windows.exe"
     $json = (Invoke-WebRequest -Uri $releases -UseBasicParsing).Content | ConvertFrom-Json 
-    $json.data.releases.'Windows/x86_64/11/exe_local'.filename
+    #$json.data.releases.'Windows/x86_64/11/exe_local'.filename
 
     (Invoke-WebRequest -Uri $releases -UseBasicParsing).Content -match $regex_url | Out-Null
 
     return @{
         Version = $matches.VersionMajor + '.' + ($matches.VersionMinor -replace '\.','')
-        URL32   = $matches.0
+        URL32   = $matches[0]
     }
 }
 
