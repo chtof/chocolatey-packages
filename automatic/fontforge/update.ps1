14a7768eea1834d707e43d4645cd6f418f02ce16
chtof <chtof62@gmail.com>
2025-11-23 00:38:31 +0100

    Update update.ps1

--- automatic/fontforge/update.ps1
+++ automatic/fontforge/update.ps1
@@ -6,22 +6,20 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {
    return github_GetInfo -ArgumentList @{
         repository = 'fontforge/fontforge'
-        regex32    = 'FontForge-(?<Version>[\d-]+)-Windows.exe$'
+        regex64    = 'FontForge-(?<Version>[\d-]+)-Windows-x64.exe$'
    }
 }
 
 function global:au_SearchReplace {
     @{
         "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"            
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
         }
 
         "tools\chocolateyinstall.ps1" = @{            
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
         }
     }
 }
