dad00777f0db00539e64696d66ecb061ff3c37e9
Chocolatey <chocolatey@realdimensions.net>
2025-12-02 16:41:54 +0000

    AU: 1 updated - steelseries-engine [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/75ce3a724e5dd8785c8a68d381c06cf939b650e3

--- automatic/steelseries-engine/tools/chocolateyinstall.ps1
+++ automatic/steelseries-engine/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG99.0.0Setup.exe'
-  checksum      = '6662c6e5b22a1de7e1e7cb6f767f98e80fd1f2195f2f24816c7a96ded7efc66a'
+  url           = 'https://engine.steelseriescdn.com/SteelSeriesGG100.0.0Setup.exe'
+  checksum      = '420664431a22c05521fa41257ce963359e930b492bd776cc6f737ab1c61bdb3e'
   checksumType  = 'sha256'
 
   silentArgs    = '/S'
