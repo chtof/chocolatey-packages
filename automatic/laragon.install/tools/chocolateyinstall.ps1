121445f9a32b514422300b585afb47467813c70a
Chocolatey <chocolatey@realdimensions.net>
2025-12-11 00:41:22 +0000

    AU: 2 updated - laragon laragon.install [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/2ea26b0899b0ab6beff7b48975fa2b81c261cef0

--- automatic/laragon.install/tools/chocolateyinstall.ps1
+++ automatic/laragon.install/tools/chocolateyinstall.ps1
@@ -33,9 +33,9 @@ $packageArgs = @{
   packageName   = "$env:ChocolateyPackageName"
   unzipLocation = "$toolsDir"
   fileType      = 'EXE'
-  url64         = 'https://github.com//leokhoa/laragon/releases/download/8.3.0/laragon-wamp.exe'
+  url64         = 'https://github.com//leokhoa/laragon/releases/download/8.4.0/laragon-wamp.exe'
 
-  checksum64    = 'b115f2c9328ae4b5d482557bb355ee05d919062891695a524ae386f31f43e2bc'
+  checksum64    = 'cd25d146c662ced01d0cd401249e82b13de37739447e62737f612cb4662b289c'
   checksumType  = 'sha256'
   
   silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components= /tasks=""$tasks"""
