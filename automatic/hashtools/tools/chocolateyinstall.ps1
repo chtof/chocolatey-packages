d4c46947eb5669188e2fd89f2ce18a371c3950d3
Chocolatey <chocolatey@realdimensions.net>
2025-12-18 16:42:17 +0000

    AU: 2 updated - flrig hashtools [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/d9a3f9738ce58d2163551d55a66df4b6b9d234d9

--- automatic/hashtools/tools/chocolateyinstall.ps1
+++ automatic/hashtools/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://binaryfortressdownloads.com/Download/BFSFiles/109/HashToolsSetup-4.8.2.exe'
-  checksum     = '17208e0015d636dea1ea035771dd5ccb181c60985c183619951f3527f1ba869f'
+  url          = 'https://binaryfortressdownloads.com/Download/BFSFiles/109/HashToolsSetup-4.8.3.exe'
+  checksum     = '6597c2050e1555d617bea96e2dcfaa10493a55af41889bf96c3a7d3e96700e98'
   checksumType = 'sha256'
   
   silentArgs   = '/VERYSILENT /SILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'
