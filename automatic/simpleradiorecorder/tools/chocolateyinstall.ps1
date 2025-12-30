6ad22e252a839e97c26b6ab2bb84365e022575ac
chtof <chtof62@gmail.com>
2025-12-30 21:42:04 +0100

    Update chocolateyinstall.ps1

--- automatic/simpleradiorecorder/tools/chocolateyinstall.ps1
+++ automatic/simpleradiorecorder/tools/chocolateyinstall.ps1
@@ -9,6 +9,7 @@ $packageArgs = @{
 
   url           = 'https://dennisbabkin.com/php/downloads/simple_radio_recorder.zip'
   checksum      = '84837FCDF35723D3CFF6B90E5A360DF388A9891B129A00FFEFE68C308F803518'
+  # 9d2b1a09b90a72c98591b3d8d57713a42fa8efe6ae25689900271f1ef0b03a73 on 30/12/2025
   checksumType  = 'sha256'
 }
 
