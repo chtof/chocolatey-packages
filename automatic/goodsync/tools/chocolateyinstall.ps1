fc2d7555c9ba63101f7032ee7bc0e121f7e796ae
Chocolatey <chocolatey@realdimensions.net>
2026-01-20 08:30:03 +0000

    AU: 1 updated - goodsync [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/10a18080f9feda6eb0bad67b4482bb244cd0821d

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-vsub-Server-Setup.exe'
-  checksum       = '7ec4a536cc17c31eb2b6b4966645e2b6b7439a953a84c191de8b599a3d4c6b78'
+  checksum       = 'd63bb82edb1575949e3d03bf717d8b7a5079e5b299c5d280346b905b1dd7e395'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
