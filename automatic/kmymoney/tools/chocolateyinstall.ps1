a88ecab74512922ed6d45fbe9a10a492ef6189d7
Chocolatey <chocolatey@realdimensions.net>
2026-01-22 16:27:23 +0000

    AU: 5 updated - dontsleep dontsleep.install dontsleep.portable kmymoney minetest [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/390631c6b0ee00b4cf0a85727dd8c6085e6ca28a

--- automatic/kmymoney/tools/chocolateyinstall.ps1
+++ automatic/kmymoney/tools/chocolateyinstall.ps1
@@ -3,10 +3,10 @@ $packageName= 'kmymoney'
 
 $packageArgs = @{
   packageName    = $packageName    
-  url64          = 'https://cdn.kde.org/ci-builds/office/kmymoney/5.2/windows/kmymoney-5.2-5153-windows-cl-msvc2022-x86_64.exe'  
+  url64          = 'https://cdn.kde.org/ci-builds/office/kmymoney/5.2/windows/kmymoney-5.2-5173-windows-cl-msvc2022-x86_64.exe'  
 
   checksumType   = 'sha256'
-  checksum64     = '78685ef554211d3d105c7da02e02431e3f8e9243db0273e5f28b4b805852c2a7'
+  checksum64     = '8fba5e58b3ca059e7851c5ac6f97ce130e53343864112a30d31f5d1247cef76c'
   checksumType64 = 'sha256'
   
   silentArgs     = '/S'
