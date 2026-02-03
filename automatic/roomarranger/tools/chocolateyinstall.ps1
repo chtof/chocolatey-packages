8706a196b682dfa6fd645483fbef4092455fe332
Chocolatey <chocolatey@realdimensions.net>
2026-02-03 00:28:29 +0000

    AU: 1 updated - roomarranger [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/b8d1e9f13f3297899bcda58c558675acb8d77fec

--- automatic/roomarranger/tools/chocolateyinstall.ps1
+++ automatic/roomarranger/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"  
-  file64      = "$toolsDir\rooarr1033_64bit.exe"
+  file64      = "$toolsDir\rooarr1034_64bit.exe"
   silentArgs	= '/S'
 }
 
