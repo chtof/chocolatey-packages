f573dbd2d3aaf32cc70878f4dca6f243e574c042
chtof <chtof62@gmail.com>
2026-02-11 21:42:05 +0100

    Update update.ps1

--- automatic/offset-explorer/update.ps1
+++ automatic/offset-explorer/update.ps1
@@ -2,14 +2,21 @@
 
 function global:au_GetLatest {
     $releases = 'http://www.kafkatool.com/download.html'
-    $regex    = "Offset Explorer (?<Version>[\d\.]+)<"
 
-    (Invoke-WebRequest -Uri $releases).RawContent -match $regex | Out-Null
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $regex_version = "Offset Explorer (?<Version>[\d\.]+)<"
+    $download_page.RawContent -match $regex_version | Out-Null
+    $version = $matches.Version
+
+    $regex32  = 'offsetexplorer.exe$'
+    $url32    = $download_page.links | ? href -match $regex32 | select -Last 1
+    $regex64  = 'offsetexplorer_64bit.exe$'
+    $url64    = $download_page.links | ? href -match $regex64 | select -Last 1
 
     return @{
-        Version = $matches.Version
-        URL32   = "https://www.kafkatool.com/download2/offsetexplorer.exe"
-        URL64   = "https://www.kafkatool.com/download2/offsetexplorer_64bit.exe"
+        Version = $version
+        URL32   = 'http://www.kafkatool.com' + $url32.href
+        URL64   = 'http://www.kafkatool.com' + $url64.href
      }
 }
 
