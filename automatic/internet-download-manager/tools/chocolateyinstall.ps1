b7f6107d833e5dfd24dfd209e14bb53550310bfb
Chocolatey <chocolatey@realdimensions.net>
2026-02-18 00:27:59 +0000

    AU: 2 updated - awscli-session-manager internet-download-manager [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/2204d5ffbbbd30f8a9088604476523b96e71b352

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman642build58.exe'
-  checksum      = 'ecde2f3e0df72061db4c9a871ba8d21d2fe08e85aa74fef56c764f1c3f02f8e8'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman642build60.exe'
+  checksum      = '48fee0be085ce2f1ba328d72538fb117c2fb965fd1827060ea3d520422362900'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
