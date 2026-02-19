35c5737c766d911bf21cbcf00b8591c1ac84c774
chtof <chtof62@gmail.com>
2026-02-19 22:11:25 +0100

    Update chocolateyinstall.ps1

--- automatic/hugin.install/tools/chocolateyinstall.ps1
+++ automatic/hugin.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   filetype      = 'MSI'    
-  file          = "$toolsDir\"
+  file64        = "$toolsDir\Hugin-2025.0.1-win64.msi"
   silentArgs	= "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
