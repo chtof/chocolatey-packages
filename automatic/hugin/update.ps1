442d40e99ccb08f7945532f876e9b280cbbeb51d
chtof <chtof62@gmail.com>
2026-02-19 22:11:42 +0100

    Update update.ps1

--- automatic/hugin/update.ps1
+++ automatic/hugin/update.ps1
@@ -1,23 +1,11 @@
-﻿import-module au
-
-[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-
-function global:au_GetLatest {
-    $releases = 'http://hugin.sourceforge.net/download'
-    $regex    = 'https://sourceforge.net/projects/hugin/files/hugin/hugin-[\d\.]+/Hugin-(?<Version>[\d\.]+)-win64.msi/download'
-
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $url = $download_page.links | ? href -match $regex
-
-     return @{ Version = $matches.Version ; URL64= $url.href }
-}
+﻿. $PSScriptRoot\..\hugin.install\update.ps1
 
 function global:au_SearchReplace {
-    @{
-        "$($Latest.PackageName).nuspec" = @{         
-            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"[$($Latest.Version)]`""
+   @{
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<dependency .+?`"$($Latest.PackageName).install`" version=)`"([^`"]+)`"" = "`$1`"$($Latest.Version)`""
         }
     }
 }
 
-update
+update -ChecksumFor none
