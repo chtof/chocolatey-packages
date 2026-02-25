b283663d0256e86c239f3fd08ceea52314686dca
Chocolatey <chocolatey@realdimensions.net>
2026-02-25 00:33:53 +0000

    AU: 6 updated - gbstudio gbstudio.install gbstudio.portable gridtracker kdevelop kdevelop.install [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/183619819c1fe0200d88abb41ce1a24c11bcbacf

--- automatic/gbstudio.install/tools/chocolateyinstall.ps1
+++ automatic/gbstudio.install/tools/chocolateyinstall.ps1
@@ -5,12 +5,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
 
-  url           = 'https://github.com//chrismaltby/gb-studio/releases/download/v4.2.1/gb-studio-windows-32bit-installer.zip'
-  checksum      = 'aabd5c92d59fde860b7029d5fb7eb1608eeb29b221752acde6dec934399c23bf'
+  url           = 'https://github.com//chrismaltby/gb-studio/releases/download/v4.2.2/gb-studio-windows-32bit-installer.zip'
+  checksum      = 'eb5832495de79193a5dbbd1fd24e173d49b38a9dca9b5b72b7d372d66f6809ee'
   checksumType  = 'sha256'
   
-  url64         = 'https://github.com//chrismaltby/gb-studio/releases/download/v4.2.1/gb-studio-windows-64bit-installer.zip'
-  checksum64    = '3f0758d0464139c5858f268bec2975c419bbf4eb9efb77a6a68f7d47ecf105fc'
+  url64         = 'https://github.com//chrismaltby/gb-studio/releases/download/v4.2.2/gb-studio-windows-64bit-installer.zip'
+  checksum64    = '412f3b90677b5dae491b18db6a64232578025a9b3f1541378561dab699f845df'
   checksumType64= 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
