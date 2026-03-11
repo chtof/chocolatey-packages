dc73fd2fd101ed9f5ab423a050c714e89c83a433
Chocolatey <chocolatey@realdimensions.net>
2026-03-11 16:42:08 +0000

    AU: 2 updated - eclipse-cpp eclipse-java-oxygen [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/9dd42ee27de7013e0dcfbbd5f7beffdaeed3d5ae

--- automatic/eclipse-java-oxygen/tools/chocolateyinstall.ps1
+++ automatic/eclipse-java-oxygen/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = "$toolsDir"
 
-  url           = 'http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/2025-12/R/eclipse-java-2025-12-R-win32-x86_64.zip'
-  checksum      = '86034a1d246882f3e22ac8bc65465b89d38f2eadff64754d0d3c294bbf5d8ff2'
+  url           = 'http://ftp-stud.fht-esslingen.de/pub/Mirrors/eclipse/technology/epp/downloads/release/2026-03/R/eclipse-java-2026-03-R-win32-x86_64.zip'
+  checksum      = '723de8f82738b4e155107fd30810e7863adaea3a2311dc9e812828d789a29a00'
   checksumType  = 'sha256'
 }
 
