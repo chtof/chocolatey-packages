3860a41c2afa9d3d9f89313a2563d3a24401f1eb
Chocolatey <chocolatey@realdimensions.net>
2026-03-19 16:42:03 +0000

    AU: 4 updated - geogebra6 geogebra-geometry sd steamachievementmanager [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/ac2b2426d1ba993cf31fe933972045d8f2006127

--- automatic/geogebra-geometry/tools/chocolateyinstall.ps1
+++ automatic/geogebra-geometry/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file     = 'GeoGebraGeometry-Windows-Installer-6-0-919-1.exe'
+$file     = 'GeoGebraGeometry-Windows-Installer-6-0-920-0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
--- automatic/geogebra6/tools/chocolateyinstall.ps1
+++ automatic/geogebra6/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$file     = 'GeoGebraGraphing-Windows-Installer-6-0-919-1.exe'
+$file     = 'GeoGebraGraphing-Windows-Installer-6-0-920-0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
