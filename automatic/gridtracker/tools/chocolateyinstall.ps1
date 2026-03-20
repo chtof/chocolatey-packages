dfcb60491dc98740b361de83c0a72d7453c1291f
Chocolatey <chocolatey@realdimensions.net>
2026-03-20 00:41:25 +0000

    AU: 3 updated - ffmpeg-batch ffmpeg-batch.install gridtracker [skip ci]

--- automatic/gridtracker/tools/chocolateyinstall.ps1
+++ automatic/gridtracker/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://download2.gridtracker.org/GridTracker2-2.260315.2-setup.exe'
-  checksum     = '3c15480fbaa66eab93cde564036a167a2db999892b2ccebf6c1b9b2c3d0ff9b8'
+  url          = 'https://download2.gridtracker.org/GridTracker2-2.260318.1-setup.exe'
+  checksum     = 'f9813dd5d832ef7fde78b65f4a7cf2f82ed404ae92583728c9cd9a63ff451cef'
   checksumType = 'sha256'
 
   silentArgs   = '/S'
