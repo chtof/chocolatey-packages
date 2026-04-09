a31cc29072a2b536419815b7c8e67faa4f2cb12b
Chocolatey <chocolatey@realdimensions.net>
2026-04-09 08:41:37 +0000

    AU: 3 updated - kdevelop kdevelop.install openssl [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/12f0292b4fb107ccffc26bbe79b47dc2cfb9b3e9

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6514-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = 'bfa4eb9b03896887332fdfc980ba58f28da888c3972e399089d6b2a95f32ea14'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6525-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = '7f82cd90f6587c53f7875c8ed799c583b3029978ad4833f7d2967d2e87de281c'
   checksumType64 = 'sha256'
 }
 
--- automatic/openssl/tools/chocolateyinstall.ps1
+++ automatic/openssl/tools/chocolateyinstall.ps1
@@ -3,13 +3,13 @@
 $packageArgs = @{
   packageName     = $env:ChocolateyPackageName
   
-  url32           = 'https://slproweb.com/download/Win32OpenSSL-3_6_1.exe'  
+  url32           = 'https://slproweb.com/download/Win32OpenSSL-3_6_2.exe'  
   checksumType32  = 'sha512'
-  checksum32      = '1f877eac8aa79a63679e5fe9522b47b431a609f6331f7b90ee9677a0238d95e7d39541e63a87165621447a6f1da2ca610d2a5cad58ddc7c9b7603b38eec6505f'
+  checksum32      = '7387107bce216eb608b132031a38990c50ac811ef71bbc10d32879583e447f8fcbe858a7c28d2c41cc9eb10cdd3f506e11a19aea36933b1587b2b53a672cc404'
 
-  url64           = 'https://slproweb.com/download/Win64OpenSSL-3_6_1.exe'
+  url64           = 'https://slproweb.com/download/Win64OpenSSL-3_6_2.exe'
   checksumType64  = 'sha512'
-  checksum64      = '190ad19fa2186a0095a920c0b19583e4f324893fdc2e95b9b01eab0ae029b8af646ded2fcebab9c09eec5c2afc8289ced70d47b9d3ff55b881b03d58c88b1580'
+  checksum64      = 'bdeaf4e8e7dc50d48cb340b180cb279354a05b3f8448136e6d6cc2ae17477515299b9d4bc4a16e41edf2a786f25c7cae039067b39094095117285565d0040dfb'
   silentArgs      = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
