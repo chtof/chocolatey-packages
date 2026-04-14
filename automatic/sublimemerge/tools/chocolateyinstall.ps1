f3bef289c3d673fff130b34611328ff6d928fb97
Chocolatey <chocolatey@realdimensions.net>
2026-04-14 08:41:41 +0000

    AU: 3 updated - spacedesk-client spacedesk-server sublimemerge [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/9a13f2960f670f4732ed36c2aecf5ecd038336d2

--- automatic/sublimemerge/tools/chocolateyinstall.ps1
+++ automatic/sublimemerge/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url64         = 'https://download.sublimetext.com/sublime_merge_build_2124_x64_setup.exe'
-  checksum64    = 'd8d2ff3f431cab650d5f7244a4b79ea47721520c29687ad2d81ec431a5605213'
+  url64         = 'https://download.sublimetext.com/sublime_merge_build_2125_x64_setup.exe'
+  checksum64    = 'e237fb46018ff4f5182ba464cf4ad81147900b812dd801f0e01bc8c8547f494e'
   checksumType  = 'sha256'
    
   silentArgs	= "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
