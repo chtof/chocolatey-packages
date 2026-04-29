0497022eb7a7b4ae3f1c2c289dc141c116ce9943
Chocolatey <chocolatey@realdimensions.net>
2026-04-29 00:46:06 +0000

    AU: 3 updated - kdevelop kdevelop.install virtualmidisynth [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/4a9daba4c12d3eb16f54275e69671d0d5de5373c

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6577-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = '529ed655520c9d79fb78964bcde7ff979256f0f3ac9309df6a370764a2a4bcdb'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6588-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = 'b422821350ae85de76ed40bf04bc7652b62f208e2b785acf860b2cf72947660f'
   checksumType64 = 'sha256'
 }
 
