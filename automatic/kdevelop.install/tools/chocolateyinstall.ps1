6e63043dcba6a2b5568dcb42fbf089fab22a0ec5
Chocolatey <chocolatey@realdimensions.net>
2026-05-04 00:41:23 +0000

    AU: 3 updated - kdevelop kdevelop.install poppler [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/d867b7991f9114246c7500eb3a745250b29e4852

--- automatic/kdevelop.install/tools/chocolateyinstall.ps1
+++ automatic/kdevelop.install/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $params = @{
   fileType       = 'EXE'
   silentArgs     = '/S'   
 
-  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6588-windows-cl-msvc2022-x86_64.exe'
-  checksum64     = 'b422821350ae85de76ed40bf04bc7652b62f208e2b785acf860b2cf72947660f'
+  url64bit       = 'https://cdn.kde.org/ci-builds/kdevelop/kdevelop/master/windows/kdevelop-master-6600-windows-cl-msvc2022-x86_64.exe'
+  checksum64     = 'dfa602967abbc90c3807b2bcfe965c2dc943c72afacdb4ad6f85bed73c21e752'
   checksumType64 = 'sha256'
 }
 
