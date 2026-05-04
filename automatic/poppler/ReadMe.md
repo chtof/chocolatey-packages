7f6670f2e189fa5163a327cf30f0adbea911b0e4
NFSL2001 <33471049+NightFurySL2001@users.noreply.github.com>
2026-05-04 15:02:12 +0800

    bump to 26.02

--- automatic/poppler/ReadMe.md
+++ automatic/poppler/ReadMe.md
@@ -1,3 +1,3 @@
 ﻿# [Poppler](https://chocolatey.org/packages/poppler)
 
-Poppler is a PDF rendering library based on the [xpdf-3.0](http://www.foolabs.com/xpdf) code base.
+Poppler is a PDF rendering library based on the [xpdf-3.0](http://www.foolabs.com/xpdf) code base. This version install the packaged zip from [oschwartz10612/poppler-windows](https://github.com/oschwartz10612/poppler-windows) which is built by [conda-forge/poppler-feedstock](https://github.com/conda-forge/poppler-feedstock).
--- automatic/poppler/tools/chocolateyinstall.ps1
+++ automatic/poppler/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url            = 'https://github.com/oschwartz10612/poppler-windows/releases/download/v25.12.0-0/Release-25.12.0-0.zip'
-$checksum       = '9499c7474e4deb41c80ef5ea4a18cc1f3843695fbfa3c247db5c46c6eab2e26f'
+$url            = 'https://github.com/oschwartz10612/poppler-windows/releases/download/v26.02.0-0/Release-26.02.0-0.zip'
+$checksum       = '993e4a94376ed712fafc7058d724ea0b943d118bbd2305cd9ed55174eb85cda5'
 $checksumType   = 'sha256'
 
 $packageArgs = @{
