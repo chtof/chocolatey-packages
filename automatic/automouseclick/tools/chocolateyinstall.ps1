e85056c45c240ed7be48efbf7ea785444927c289
Chocolatey <chocolatey@realdimensions.net>
2026-05-08 16:50:37 +0000

    AU: 2 updated - automouseclick dust3d [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/8b1561ee0e53f17521728892ca153d5d9fa95f00

--- automatic/automouseclick/tools/chocolateyinstall.ps1
+++ automatic/automouseclick/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://www.murgee.com/auto-mouse-click/download/setup.exe'
-  checksum     = '36AED8A98EB719F042B1F3EBEE8252F47641A52611CB604053B88A03E5840E3C'
+  checksum     = 'CBA7B1BB9D7754AE48CCC368504673B800B59458C333256E8DAC64F8D1E7AC1E'
   checksumType = 'sha256'
 
   silentArgs   = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
