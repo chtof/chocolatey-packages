d665c207fedb098d6cd95a50d7a4abfd1dc460cf
chtof <chtof62@gmail.com>
2026-05-13 22:47:29 +0200

    Update update.ps1

--- automatic/dust3d/update.ps1
+++ automatic/dust3d/update.ps1
@@ -6,24 +6,20 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 function global:au_GetLatest {
    return github_GetInfo -ArgumentList @{
         repository = 'huxingyi/dust3d'
-        regex32    = 'dust3d-[\d\.]+(-beta|-rc)?\.[\d\.]*(-x86)?.zip'
-        #regex64    = 'dust3d-(?<Version>[\d\.]+)(-beta|-rc)?(?<VersionBeta>\.[\d\.]*).zip'        
+        regex32    = 'dust3d-[\d\.]+(-beta|-rc)?\.[\d\.]*(-x86)?.zip'        
    }
 }
 
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"            
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
-            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"            
         }
 
         "tools\chocolateyinstall.ps1" = @{
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
-          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""          
         }
     }
 }
