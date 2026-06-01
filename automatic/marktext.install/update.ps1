c487810f9d97f0c7fd830aceee6572ecf9833e28
chtof <chtof62@gmail.com>
2026-06-01 22:02:25 +0200

    Update update.ps1

--- automatic/marktext.install/update.ps1
+++ automatic/marktext.install/update.ps1
@@ -1,27 +1,26 @@
 ﻿import-module au
 . ..\..\helpers\GitHub_Helper.ps1
 
-function global:au_BeforeUpdate() {
-  $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+function global:au_BeforeUpdate() {  
   $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
 }
 
 function global:au_GetLatest {
    return github_GetInfo -ArgumentList @{
         repository = 'marktext/marktext'
-        regex32    = '/v(?<Version>[\d\.]*)/.*.exe$'        
+        regex64    = '/v(?<Version>[\d\.]*)/.*.exe$'        
    }
 }
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyinstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')"      = "`${1}'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum32)'"
+			"(^(\s)*url64\s*=\s*)('.*')"      = "`${1}'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum64)'"
         }        
     }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update
+    update -ChecksumFor 64
 }
