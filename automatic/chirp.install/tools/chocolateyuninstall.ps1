fc8b8f6e3f2408ad7a0d7ff016f5174f3543a688
chtof <chtof62@gmail.com>
2026-06-01 23:12:51 +0200

    No longer available as filtered by CloudFlare.

--- automatic/chirp.install/tools/chocolateyuninstall.ps1
+++
@@ -1,8 +0,0 @@
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  file			= (Get-ItemProperty HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\CHIRP UninstallString).UninstallString;
-  fileType		= 'exe'
-  silentArgs	= '/S'
-}
-
-Uninstall-ChocolateyPackage @packageArgs
