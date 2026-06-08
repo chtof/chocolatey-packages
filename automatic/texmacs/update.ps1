5e10c6d53639b4837f71da10b035a08b7e270652
chtof <chtof62@gmail.com>
2026-06-08 22:28:32 +0200

    Update update.ps1

--- automatic/texmacs/update.ps1
+++ automatic/texmacs/update.ps1
@@ -3,8 +3,8 @@ import-module au
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-    $releases = 'http://www.texmacs.org/tmweb/download/windows.en.html'    
-    $regex    = 'TeXmacs-(?<Version>[\d\.]+)-installer.exe$'
+    $releases = 'https://www.texmacs.org/tmweb/download/windows.en.html'    
+    $regex    = 'TeXmacs-(?<Version>[\d\.]+)(-installer)?-x86_64.exe$'
 
     $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex
 
@@ -13,20 +13,18 @@ function global:au_GetLatest {
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+       "legal\VERIFICATION.txt"  = @{       
             "(?i)(x64: ).*"             = "`${1}$($Latest.URL32)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"            
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum32)"
         }
 
         "tools\chocolateyinstall.ps1" = @{
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
+    update -ChecksumFor 64
 }
