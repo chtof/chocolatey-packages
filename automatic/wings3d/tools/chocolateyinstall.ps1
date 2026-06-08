adc8e3820e1ff2152166f494e44d19e5f4f4a54f
chtof <chtof62@gmail.com>
2026-06-08 22:08:17 +0200

    Update chocolateyinstall.ps1

--- automatic/wings3d/tools/chocolateyinstall.ps1
+++ automatic/wings3d/tools/chocolateyinstall.ps1
@@ -3,10 +3,11 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName  
-  file        = "$toolsDir\wings-2.3.exe"    
-  file64      = "$toolsDir\wings-x64-2.3.exe"    
+  file        = "$toolsDir\wings-2.4.1.exe"    
+  file64      = "$toolsDir\wings-x64-2.4.1.exe"    
   silentArgs  = '/S'
 }
 
+Start-Process 'AutoHotKey' -Verb runas -ArgumentList "`"$toolsDir\chocolateyinstall.ahk`""
 Install-ChocolateyInstallPackage @packageArgs
 Write-Warning "OpenGL 2.1 for Windows is needed to launch Wings3D. See https://www.khronos.org/opengl/wiki/Getting_Started#Downloading_OpenGL"
