17055e89d76940718edb20ba84a8bc20479cc633
chtof <chtof62@gmail.com>
2026-06-08 22:23:16 +0200

    Update chocolateyinstall.ps1

--- automatic/umbrello.install/tools/chocolateyinstall.ps1
+++ automatic/umbrello.install/tools/chocolateyinstall.ps1
@@ -5,9 +5,12 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://download.kde.org/stable/umbrello/latest/win32/umbrello-mingw32-2.31.2-lp151.49.1-setup.exe'
+  url64        = 'https://download.kde.org/stable/umbrello/latest/win32/umbrello-mingw32-2.31.2-lp151.49.1-setup.exe'
   checksum     = ''
+  checksum64     = ''
   checksumType = 'sha256'
 
+  file32       = "$toolsDir\umbrello-mingw64-2.31.2-lp151.121.1-setup.exe"
   file64       = "$toolsDir\umbrello-mingw64-2.31.2-lp151.121.1-setup.exe"
 
   silentArgs   = '/S'
