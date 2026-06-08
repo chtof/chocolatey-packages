316e288f5133ea4131ce925871bc127a0eae6f6c
chtof <chtof62@gmail.com>
2026-06-08 22:28:28 +0200

    Update chocolateyinstall.ps1

--- automatic/texmacs/tools/chocolateyinstall.ps1
+++ automatic/texmacs/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
-  file        = "$toolsDir\TeXmacs-2.1.4-installer.exe"
+  file64      = "$toolsDir\TeXmacs-2.1.4-installer.exe"
   silentArgs  = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
 }
 
