9051a5f7c87a51145a801110c07658feb226e318
chtof <chtof62@gmail.com>
2026-06-08 22:41:19 +0200

    Update update.ps1

--- automatic/stremio/update.ps1
+++ automatic/stremio/update.ps1
@@ -1,16 +1,12 @@
 ﻿import-module au
 
 function global:au_GetLatest {
-    $releases      = 'https://www.stremio.com'
-    $regex         = '(https://www.strem.io/download.*)'
-    $regex_version = 'https://dl.strem.io/(shell-)?win/v[\d\.]+/Stremio\+(?<Version>[\d\.]+).exe'
+    $releases      = 'https://www.stremio.com/downloads'    
+    $regex_version = 'https://dl.strem.io/stremio-shell-ng/v[\d\.]+/StremioSetup-v(?<Version>[\d\.]+)_x64.exe'
+        
+    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex_version | Out-Null
 
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	$url = $download_page.links.href -match $regex | select -First 1
-    $url_latestVersion = Get-RedirectedUrl "$url"
-    $url_latestVersion -match $regex_version | Out-Null
-
-    return @{ Version = $matches.Version ; URL32 = $url_latestVersion }
+    return @{ Version = $matches.Version ; URL32 = $url.href }
 }
 
 function global:au_SearchReplace {
