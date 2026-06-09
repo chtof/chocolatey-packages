3a2a2cb8deb89a1db124bbcdb9c2d7d8227f511c
chtof <chtof62@gmail.com>
2026-06-09 22:52:49 +0200

    Update update.ps1

--- automatic/rotpdf/update.ps1
+++ automatic/rotpdf/update.ps1
@@ -3,17 +3,17 @@ import-module au
 
 function global:au_GetLatest {
     $releases     = 'https://www.ne.jp/asahi/foresth/home/'
-    $regexVersion = 'RotPDF Ver(?<Version>[\d\._]+)'
-	$regex        = 'rpdf(?<Version>[\d\._]+).zip'
+    $regexVersion = 'RotPDF.*?\r?\n\s*Ver(?<version>\d+\.\d+)'    
+	$regex        = 'rpdf[\d\._]+.zip'
  
 	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 	$url = $download_page.links | ? href -match $regex | select -Last 1
+    $download_page -match $regexVersion
 
-    $HTML = New-Object -Com "HTMLFile"
-    $HTML.IHTMLDocument2_write($download_page.Content)
-    ($HTML.all.tags("strong") |% InnerText | Out-String) -match $regexVersion | Out-Null
-
-     return @{ Version = $matches.Version ; URL32 = $releases + $url.href }
+    return @{
+        Version = $matches.version
+        URL32 = $releases + $url.href
+    }
 }
 
 function global:au_SearchReplace {
