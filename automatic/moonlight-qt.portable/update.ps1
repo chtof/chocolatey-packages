6a3a49308fba29447e4940177e13270e0283e5db
chtof <chtof62@gmail.com>
2026-06-10 22:26:36 +0200

    Update update.ps1

--- automatic/moonlight-qt.portable/update.ps1
+++ automatic/moonlight-qt.portable/update.ps1
@@ -4,39 +4,33 @@ function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
     $github_repository = "moonlight-stream/moonlight-qt"
-    $releases          = "https://github.com/" + $github_repository + "/releases/latest"
-    $regex32           = 'MoonlightPortable-x86-(?<Version>[\d\.]+).zip'
-    $regex64           = 'MoonlightPortable-x64-([\d\.]+).zip'
+    $releases          = "https://github.com/" + $github_repository + "/releases/latest"    
+    $regex64           = 'MoonlightPortable-x64-(?<Version>[\d\.]+).zip'
 
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	$url32         = $download_page.links | ? href -match $regex32 | Select -First 1
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing	
     $version       = $matches.Version
     $url64         = $download_page.links | ? href -match $regex64 | Select -First 1
 
     return @{
-        Version = $version
-        URL32   = $url32.href
+        Version = $version        
         URL64   = $url64.href
     }
 }
 
 function global:au_SearchReplace {
     @{
-       "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+       "legal\VERIFICATION.txt"  = @{                        
             "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
-            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
-            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
-        "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+        "tools\chocolateyinstall.ps1" = @{          
           "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`"" = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
 if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
-    update -ChecksumFor none
+    update -ChecksumFor 64
 }
