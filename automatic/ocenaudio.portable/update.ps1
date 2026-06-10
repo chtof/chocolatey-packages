5841f1a038e734f6222178977cec954fa5beb255
chtof <chtof62@gmail.com>
2026-06-10 22:53:41 +0200

    Update update.ps1

--- automatic/ocenaudio.portable/update.ps1
+++ automatic/ocenaudio.portable/update.ps1
@@ -1,7 +1,6 @@
 import-module au
 
-function global:au_BeforeUpdate() {
-  $Latest.Checksum32 = Get-RemoteChecksum $Latest.Url32
+function global:au_BeforeUpdate() {  
   $Latest.Checksum64 = Get-RemoteChecksum $Latest.Url64
 }
 
@@ -9,20 +8,17 @@ function global:au_GetLatest {
     $releases = 'https://www.ocenaudio.com'
     $regex    = '\<h1\>ocenaudio (?<Version>[\d\.]+)\</h1\>'
 
-    (Invoke-WebRequest -Uri $releases).Content  -match $regex | Out-Null
+    (Invoke-WebRequest -Uri $releases -UseBasicParsing).Content  -match $regex | Out-Null
 
     return @{
-        Version = $matches.Version
-        URL32   = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_portable.zip'
-        URL64   = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio64_portable.zip'
+        Version = $matches.Version        
+        URL64   = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_windows64.zip'
     }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        "tools\chocolateyInstall.ps1" = @{			
             "(^(\s)*url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
             "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
