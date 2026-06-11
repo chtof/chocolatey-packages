0004e5875ff5026c9dcd7ef72c7e5a84e1f44cc4
chtof <chtof62@gmail.com>
2026-06-11 22:07:55 +0200

    Update update.ps1

--- automatic/openmodelica/update.ps1
+++ automatic/openmodelica/update.ps1
@@ -5,25 +5,20 @@ import-module au
 function global:au_GetLatest {
   $releases = 'https://openmodelica.org/download/download-windows'
   $regex    = '/windows/releases/(?<Version>[\d\./]+).*$'
-  # $regexVersion      = '<a href="https://build.openmodelica.org/omc/builds/windows/(releases/)?.*>(?<Version>[\d\.]+)(/[\d]+)?<br />'
-  # <a href="https://build.openmodelica.org/omc/builds/windows/releases/1.14/final/"><span style="font-size: 13.3333px;">1.14.0<br />
-
+  
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
   $url           = $download_page.links | ? href -match $regex | Select -First 1
   $version       = $matches.Version -Replace '/', '.'
 
 	return @{
-    Version = $version
-    URL32   = $url.href + '/32bit/OpenModelica-v' + $version + '-32bit.exe'
+    Version = $version    
     URL64   = $url.href + '/64bit/OpenModelica-v' + $version + '-64bit.exe'
   }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyinstall.ps1" = @{
-          "(^(\s)*url\s*=\s*)('.*')"        = "`${1}'$($Latest.URL32)'"
-          "(^(\s)*checksum\s*=\s*)('.*')"   = "`${1}'$($Latest.Checksum32)'"
+        "tools\chocolateyinstall.ps1" = @{          
           "(^(\s)*url64\s*=\s*)('.*')"      = "`${1}'$($Latest.URL64)'"
           "(^(\s)*checksum64\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum64)'"
         }
