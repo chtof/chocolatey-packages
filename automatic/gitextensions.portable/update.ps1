e08609485f598432acad7824f647d3b6e7cbd7da
chtof <chtof62@gmail.com>
2026-06-12 21:35:56 +0200

    Update update.ps1

--- automatic/gitextensions.portable/update.ps1
+++ automatic/gitextensions.portable/update.ps1
@@ -13,15 +13,15 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     @{
        "legal\VERIFICATION.txt"  = @{                        
-            "(?i)(x64: ).*"               = "`${1}$($Latest.URL64)"            
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"            
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"            
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
 
-        "tools\chocolateyinstall.ps1" = @{        
-          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`${1}$($Latest.FileName64)`""
+        "tools\chocolateyinstall.ps1" = @{          
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
         }
     }
 }
 
-update -ChecksumFor 64
+update -ChecksumFor None
