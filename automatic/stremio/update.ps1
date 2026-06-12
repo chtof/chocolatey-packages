ca9d3a937d8e1dee4d31d7b0449401a17f633178
chtof <chtof62@gmail.com>
2026-06-12 22:01:00 +0200

    Update update.ps1

--- automatic/stremio/update.ps1
+++ automatic/stremio/update.ps1
@@ -1,21 +1,24 @@
-﻿import-module au
+import-module au
 
 function global:au_GetLatest {
     $releases      = 'https://www.stremio.com/downloads'    
     $regex_version = 'https://dl.strem.io/stremio-shell-ng/v[\d\.]+/StremioSetup-v(?<Version>[\d\.]+)_x64.exe'
-        
-    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex_version | Out-Null
 
-    return @{ Version = $matches.Version ; URL32 = $url.href }
+    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex_version
+
+    return @{
+        Version = $matches.Version
+        URL64 = $url.href
+    }
 }
 
 function global:au_SearchReplace {
     @{
-        "tools\chocolateyInstall.ps1" = @{
-			"(^(\s)*url\s*=\s*)('.*')"      = "`${1}'$($Latest.URL32)'"
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum32)'"
+        "tools\chocolateyinstall.ps1" = @{
+            "(^(\s)*url64\s*=\s*)('.*')"      = "`${1}'$($Latest.URL64)'"
+            "(^(\s)*checksum64\s*=\s*)('.*')" = "`${1}'$($Latest.Checksum64)'"
         }
     }
 }
 
-update
+update -ChecksumFor 64
