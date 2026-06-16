34b66bf037867b8ec20d49ef422cded783be8b79
chtof <chtof62@gmail.com>
2026-06-16 22:18:48 +0200

    Update update.ps1

--- automatic/poppler/update.ps1
+++ automatic/poppler/update.ps1
@@ -1,25 +1,19 @@
 ﻿import-module au
+. ..\..\helpers\GitHub_Helper.ps1
 
 function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
 
 function global:au_GetLatest {
-    #$releases = 'https://anaconda.org/conda-forge/poppler/files?sort=basename&sort_order=desc'
-    $releases = 'https://poppler.freedesktop.org'
-	$regex   = 'poppler-(?<Version>[\d\.]+)(-.*)?.tar.(bz2|xz)$'
-	
-
-    $url = (Invoke-WebRequest -Uri $releases -UseBasicParsing).links | ? href -match $regex | Select -First 1
-
-    return @{
-        Version = $matches.Version
-        URL64   = 'https://poppler.freedesktop.org/' + $url.href
-    }
+   return github_GetInfo -ArgumentList @{
+        repository = 'oschwartz10612/poppler-windows'
+        regex64    = '/Release-(?<Version>[\d\.-]+).zip'
+   }
 }
 
 function global:au_SearchReplace {
     @{
         "legal\VERIFICATION.txt"  = @{            
-            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"            
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
             "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType64)"
             "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
         }
