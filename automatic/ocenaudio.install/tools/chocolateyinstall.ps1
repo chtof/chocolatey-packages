c6bc0fad28b613f46a80d8ea1713009da204ca62
Chocolatey <chocolatey@realdimensions.net>
2026-06-19 20:37:01 +0000

    AU: 2 updated - ocenaudio.install openmodelica [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/b1bd8b2d8773bbf9637e0b76ce64927924215891

--- automatic/ocenaudio.install/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
     
   url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_windows64.exe'
-  checksum64     = '3a8d80a904b37103ec03d0226acfa75addc287201e02def009cd0c7f32b57586'
+  checksum64     = '5154d7f161eeba2652ef0381f48a4811ed97df92d27d00342fdf0a30c98a1772'
   checksumType64 = 'sha256'
 
   silentArgs     = "/S"
--- automatic/openmodelica/tools/chocolateyinstall.ps1
+++ automatic/openmodelica/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
     
-  url64         = 'https://build.openmodelica.org/omc/builds/windows/releases/1.22/3/64bit/OpenModelica-v1.22.3-64bit.exe'
-  checksum64    = 'dad49569f472f4c3ae1f539958573531ab408ec876c4b38fab5b3add3c2e7c47'
+  url64         = 'https://build.openmodelica.org/omc/builds/windows/releases/1.26/9/64bit/OpenModelica-v1.26.9-64bit.exe'
+  checksum64    = 'ede0201609835cb42d2f6d92e4087e528016f26fef23eda5e652e364f8058bee'
   checksumType64= 'sha256'
   
   silentArgs	= '/S'
