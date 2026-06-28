e96d2c22f3bcbfd921924688fd391894ff5a27aa
Chocolatey <chocolatey@realdimensions.net>
2026-06-28 16:26:59 +0000

    AU: 4 updated - internet-download-manager phonerlite phonerlite.install phonerlite.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/5ee022ecf585a3792f004fbadbe60918a1a07bb4

--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman643build1.exe'
-  checksum      = 'ffcda850d884d036270142850985b48112ce8b73ee4416b3eb91739cb9ff0065'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman643build2.exe'
+  checksum      = '22bc3ab4c0a96263eeba29cf3d61e0347945ff875a30b771512f86c533820e84'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
