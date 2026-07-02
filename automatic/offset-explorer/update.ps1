894a1ebc21bcf0cc188cbd6ce472f54422647b8a
Jaap Spiering <jaap@jssd.nl>
2026-07-02 10:15:36 +0200

    Update download location for offset-explorer, 32bit version is no longer offered
    Update version and copyright

--- automatic/offset-explorer/update.ps1
+++ automatic/offset-explorer/update.ps1
@@ -8,14 +8,11 @@ function global:au_GetLatest {
     $download_page.RawContent -match $regex_version | Out-Null
     $version = $matches.Version
 
-    $regex32  = 'offsetexplorer.exe$'
-    $url32    = $download_page.links | ? href -match $regex32 | select -Last 1
     $regex64  = 'offsetexplorer_64bit.exe$'
     $url64    = $download_page.links | ? href -match $regex64 | select -Last 1
 
     return @{
         Version = $version
-        URL32   = 'http://www.kafkatool.com' + $url32.href
         URL64   = 'http://www.kafkatool.com' + $url64.href
      }
 }
@@ -23,7 +20,6 @@ function global:au_GetLatest {
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(^(\s)*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
             "(^(\s)*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
