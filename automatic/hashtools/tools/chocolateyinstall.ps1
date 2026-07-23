77860a3e432d324d840e4818cbe124d25282fe03
Chocolatey <chocolatey@realdimensions.net>
2026-07-23 00:29:01 +0000

    AU: 6 updated - hashtools internet-download-manager jami pyzo pyzo.install pyzo.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/6171ad246109c04624c78cb42b5096533cdd306e

--- automatic/hashtools/tools/chocolateyinstall.ps1
+++ automatic/hashtools/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://binaryfortressdownloads.com/Download/BFSFiles/109/HashToolsSetup-4.8.3c.exe'
-  checksum     = '0d9c4ab08d6d9b4d38ed0a0320a030bf73f78c6be291e750a400fc5be17cc529'
+  url          = 'https://binaryfortressdownloads.com/Download/BFSFiles/109/HashToolsSetup-4.8.4.exe'
+  checksum     = 'f3ba1f1a952a4d26e85078b8a1b0d087f91a65a51300775c37d4e569da384b31'
   checksumType = 'sha256'
   
   silentArgs   = '/VERYSILENT /SILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'
--- automatic/internet-download-manager/tools/chocolateyinstall.ps1
+++ automatic/internet-download-manager/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
 
-  url           = 'http://mirror2.internetdownloadmanager.com/idman643build6.exe'
-  checksum      = '5f2215b9c145c89ebfbb2192d087e0a044b24d0602007437ebf79d94e8e38940'
+  url           = 'http://mirror2.internetdownloadmanager.com/idman643build7.exe'
+  checksum      = '006f87a8cd6aefa83185b8abbf7057f8c2fe2d5063da3b224f61fb68bea4cff7'
   checksumType  = 'sha256'
 
   silentArgs	= "/skipdlgs"
