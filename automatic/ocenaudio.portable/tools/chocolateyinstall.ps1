95eddc560c9dbe0691388ea032341dd0ba0b6ad7
Chocolatey <chocolatey@realdimensions.net>
2026-08-03 00:28:32 +0000

    AU: 1 updated - ocenaudio.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/86139ada8664ffe322ec2b1c2ab873b5dcbf8da7

--- automatic/ocenaudio.portable/tools/chocolateyinstall.ps1
+++ automatic/ocenaudio.portable/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   unzipLocation  = "$toolsDir"
 
   url64          = 'https://www.ocenaudio.com/downloads/index.php/ocenaudio_windows64.zip'  
-  checksum64     = '44d380d9f77787cbba605c95f1433675087871480fcb5cc3de519d99bfdbbdcc'
+  checksum64     = '643227896707d6cb7ebfbe4149948209767db773f39bd1baed1dc28fa7818dda'
   checksumType64 = 'sha256'
 }
 
