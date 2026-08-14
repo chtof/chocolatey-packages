413a8bb1f9a501529e3f06ed34f029a33da874c6
Chocolatey <chocolatey@realdimensions.net>
2026-08-14 08:27:48 +0000

    AU: 2 updated - afedteated ausweisapp2 [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/d8136d31f78e610bea8309d234783d65acc5a02e

--- automatic/afedteated/tools/chocolateyinstall.ps1
+++ automatic/afedteated/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
   url          = 'https://cdn.iobit.com/dl/advanced-systemcare-setup.exe'
-  checksum     = '1bbc2bb7bff90b26a66aa84729e872e5d554274b6b285547056e4d08d5fd7f168a97ccde801d95ef6232e341cb94d4bc9a8f478957bfa82cb16c53c40ac7719d'
+  checksum     = 'f7c479246be9d198c9fe3d56d837691d0410a6406c0e2170dd345b4dd43ded23a358d604c7c50b660db162e5085209837aa21d5c6f397ffddd0449456e109389'
   checksumType = 'sha512'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
