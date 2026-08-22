5607633e2e6fde47a57005b187fd8278f66c30e9
Chocolatey <chocolatey@realdimensions.net>
2026-08-22 00:27:37 +0000

    AU: 3 updated - cudatext goodsync jami [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/f1c049669debf7a8a5b90c80040dddcc97d2abc3

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-vsub-Server-Setup.exe'
-  checksum       = '81da6291f0df3aa571ae6459175269a396a412cf802c39a684af695b34b45292'
+  checksum       = '4babe00bb76ec35556a28bff443ebba049da7aa751204525f5629a60a6913bda'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
