b0679059d740b563dabe8ed016cfce29b3a75e48
Chocolatey <chocolatey@realdimensions.net>
2026-09-15 16:30:51 +0000

    AU: 5 updated - poppler voicemeeter-banana voicemeeter-banana.install voicemeeter-banana.portable voicemeeter-potato.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/4793cb88294e6148adf3b34736af28c537ed3a7f

--- automatic/voicemeeter-banana.install/tools/chocolateyinstall.ps1
+++ automatic/voicemeeter-banana.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   destination = "$toolsDir"
-  FileFullPath= "$toolsDir\VoicemeeterSetup_v2122.zip"
+  FileFullPath= "$toolsDir\VoicemeeterSetup_v2130.zip"
 }
 Get-ChocolateyUnzip @packageArgs
 
