92fe51126f73bbc92bcaa7daf13782339bd42c45
Chocolatey <chocolatey@realdimensions.net>
2026-09-26 00:27:50 +0000

    AU: 3 updated - goodsync jtalert roomarranger [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/a47482301265b6316b0f54907f4239c50b36b0d5

--- automatic/goodsync/tools/chocolateyinstall.ps1
+++ automatic/goodsync/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
   filetype       = "MSI"
 
   url            = 'https://www.goodsync.com/download/GoodSync-vsub-Server-Setup.exe'
-  checksum       = '137e116ff0d6af1d9633b79993e2bf7e9a6d8ae4ac3ade287e75801c122e0dfe'
+  checksum       = '6d6b1c6f08768159378fae382c6326ac92089533664cbd3a6d8c15151e56fb5a'
   checksumType   = 'sha256'
 
   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
--- automatic/jtalert/tools/chocolateyinstall.ps1
+++ automatic/jtalert/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageArgs = @{
   packageName  = $env:ChocolateyPackageName
 
-  url          = 'https://dnl.hamapps.com/JTAlert/295bbf3bca1fe42cb8c0dd66486e5ac2'
-  checksum     = '389685ac33cc77d5fb5fa0aba118bdb324c029c8b4e98d075c1bafd838b5e69c'
+  url          = 'https://dnl.hamapps.com/JTAlert/5a237412805ea83188494430ce9e0e35'
+  checksum     = 'f625ee796e51bb6d5faeeb811ef538f5fa443643c7c4b9b4a89b9be6529e3a37'
   checksumType = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /components=""'
